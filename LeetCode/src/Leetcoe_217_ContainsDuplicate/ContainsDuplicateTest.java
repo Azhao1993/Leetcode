0a0a845063e6a336bd2a22977506b8acb475cb8a
andrew <1124913956@qq.com>
2018-08-09 21:05:18 +0800

    217重复的元素

---
+++ LeetCode/src/Leetcoe_217_ContainsDuplicate/ContainsDuplicateTest.java
@@ -0,0 +1,12 @@
+package Leetcoe_217_ContainsDuplicate;
+
+public class ContainsDuplicateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ContainsDuplicate cd = new ContainsDuplicate();
+		int[] nums = {3,3};
+		System.out.println(cd.containsDuplicate(nums));
+	}
+
+}
