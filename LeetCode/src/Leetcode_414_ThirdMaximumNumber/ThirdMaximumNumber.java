98378d633c085205ddd066941784a97ebc221214
andrew <1124913956@qq.com>
2018-08-10 17:28:51 +0800

    414.第三大的数

--- LeetCode/src/Leetcode_414_ThirdMaximumNumber/ThirdMaximumNumber.java
+++ LeetCode/src/Leetcode_414_ThirdMaximumNumber/ThirdMaximumNumber.java
@@ -34,17 +34,17 @@ public class ThirdMaximumNumber {
 					nums[j] = temp;
 				}
 			}
-			//从nums[1]开始检查
-			if(i>0) {
-				if(nums[i-1]!=nums[i]) {
-					//max = nums[i];
-					count++;
-					if(count==3) {
-						return nums[i];
-					}				
-				}
+		}
+		for(int i = 1;i<nums.length;i++) {
+			//从nums[1]开始检查			
+			if(nums[i-1]!=nums[i]) {
+				//max = nums[i];
+				count++;
+				if(count==3) {
+					return nums[i];
+				}				
 			}
-
+			
 		}
 		
 		return nums[0];
