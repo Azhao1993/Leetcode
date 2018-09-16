72a5bebf77409cd0d097cdcdddeb68d990d13c1d
andrew <1124913956@qq.com>
2018-09-16 16:28:27 +0800

    680.验证回文字符串Ⅱ

---
+++ LeetCode/src/Leetcode_680_ValidPalindrome2/ValidPalindrome2.java
@@ -0,0 +1,32 @@
+package Leetcode_680_ValidPalindrome2;
+/*
+	给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+	
+	示例 1:		
+		输入: "aba"
+		输出: True
+	示例 2:	
+		输入: "abca"
+		输出: True
+	解释: 你可以删除c字符。
+	注意:	
+		字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+*/
+public class ValidPalindrome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	//680. 验证回文字符串 Ⅱ
+    public boolean validPalindrome(String s) {
+        //创建字符串缓冲器
+    	StringBuilder builder = new StringBuilder();
+    	int length  =s.length();
+    	//诸位判断
+    	for(int i  =0;i<builder.length();i++) {
+    		
+    	}
+    }
+
+}
