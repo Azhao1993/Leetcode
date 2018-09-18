2343167409646b3dfb37b4ed3898143159a2b82b
andrew <1124913956@qq.com>
2018-09-18 11:01:14 +0800

    859.亲密字符串

--- LeetCode/src/Leetcode_859_BuddyStrings/BuddyStrings.java
+++ LeetCode/src/Leetcode_859_BuddyStrings/BuddyStrings.java
@@ -1,4 +1,8 @@
 package Leetcode_859_BuddyStrings;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /*
 给定两个由小写字母构成的字符串 A 和 B ，只要我们可以通过交换 A 中的两个字母得到与 B 相等的结果，就返回 true ；否则返回 false 。
 示例 1：
@@ -31,37 +35,47 @@ public class BuddyStrings {
 	}
 	//859. 亲密字符串
 	 public boolean buddyStrings(String A, String B) {
-		 if(A.length()!=B.length()){
-			return false; 
+		 //其中一个为空
+		 if((A.length()==0)||(B.length()==0)) {
+			 return false;
 		 }
-		
-		 StringBuilder abulider = new StringBuilder(A);
-		 StringBuilder bbulider = new StringBuilder(B);
-		 if(abulider.length()==2) {
-			 abulider.reverse();
-			 if(abulider.reverse().indexOf(B)!=-1) {
-				 return true;
-			 }else {
+		 //A,B长度不相等
+		 if(A.length()!=B.length()) {
+			 return false;
+		 }
+		 //A,B内容相同
+		 if(A.equals(B)) {
+			 //判断字符是否重复
+			 Set<Character> set = new HashSet<Character>();
+			 for(int i = 0;i<A.length();i++) {
+				 set.add(A.charAt(i));
+			 }
+			 //set的大小和A的长度相同，说明无重复
+			 if(set.size()==A.length()) {
+				 return false;
+			 }
+			 return true;
+		 }
+		 //内容不同
+		 //计数器
+		 int count = 0;
+		 Set<Character> seta = new HashSet<Character>();
+		 Set<Character> setb = new HashSet<Character>();
+		 for(int i = 0;i<A.length();i++) {
+			 if(A.charAt(i)!=B.charAt(i)) {
+				 count++;
+				 seta.add(A.charAt(i));
+				 setb.add(B.charAt(i));
+			 }
+			 if(count>2) {
 				 return false;
 			 }
 		 }
-		 //删除相同位置的字符
-		 for(int i = 0;i<abulider.length();) {
-			 if(abulider.charAt(i)==bbulider.charAt(i)) {
-				 abulider.deleteCharAt(i);
-				 bbulider.deleteCharAt(i);				 
-			 }else {
-				 i++;
-			 }
+		 //判断seta和setb是否相同
+		 if((count==2)&(seta.equals(setb))) {
+			 return true;
 		 }
-		 //判断长度是否为2
-		 if(abulider.length()!=2) {
-			 return false;
-		 }
-		 if(abulider.reverse().indexOf(B)==-1) {
-			 return false;
-		 }
-		 return true;
+		 return false;
 	 }
 
 }
