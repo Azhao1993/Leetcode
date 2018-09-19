6392abc96e8767de0bac04f65c0cc549326ff260
Andrew <Andrew@118.202.42.100>
2018-09-20 02:14:23 +0800

    63.不同路径 II

---
+++ LeetCode/src/UsualMethod/matrixOutput.java
@@ -0,0 +1,20 @@
+package UsualMethod;
+
+public class matrixOutput {
+	public void ArrayOutput(int[][] grid) {
+		// 获取grid的行和列
+		int row = grid.length;
+		int col = grid[0].length;
+		// 输出数组
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (j != col - 1) {
+					System.out.print(grid[i][j] + ",");
+				}else {
+					System.out.println(grid[i][j]);
+				}
+
+			}
+		}
+	}
+}
