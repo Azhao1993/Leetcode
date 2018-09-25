4c4b22802d800128bf9ef7be73de22bbd7f714e2
andrew <1124913956@qq.com>
2018-09-25 14:04:32 +0800

    X的平方根

---
+++ LG/Binary Search/SqrtX.java
@@ -0,0 +1,42 @@
+package BinarySearch_069_SqrtX;
+
+/*
+	实现 int sqrt(int x) 函数。	
+	计算并返回 x 的平方根，其中 x 是非负整数。	
+	由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+	
+	示例 1:	
+		输入: 4
+		输出: 2
+	示例 2:	
+		输入: 8
+		输出: 2
+		说明: 8 的平方根是 2.82842..., 
+		     由于返回类型是整数，小数部分将被舍去。
+*/
+public class SqrtX {
+	public static void main(String[] args) {
+		SqrtX sx = new SqrtX();
+		System.out.println(sx.mySqrt(9));
+	}
+	public int mySqrt(int x) {
+		// 小于1
+		if (x <= 1) {
+			return 1;
+		}
+		// 0-x二分查找
+		int left = 0;
+		int right = x;	
+		while (left < right) {
+			int mid = (left + right) / 2;
+			if(x/mid>=mid) {
+				//mid太小了
+				left = mid +1;
+			}else{
+				//mid太大了
+				right = mid;
+			}
+		}
+		return right-1;
+	}
+}
