ce1bbc7222d0c4d4e11b502553353216d26d52d1
andrew <1124913956@qq.com>
2018-09-25 18:17:17 +0800

    33.搜索旋转排序数组

--- ProblemSet/src/Leetcode_33_SearchinRotatedSortedArray/SearchinRotatedSortedArray.java
+++ ProblemSet/src/Leetcode_33_SearchinRotatedSortedArray/SearchinRotatedSortedArray.java
@@ -19,15 +19,46 @@ public class SearchinRotatedSortedArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SearchinRotatedSortedArray srsa = new SearchinRotatedSortedArray();
+		int[] nums = {4,5,6,7,0,1,2};
+		int target = 0;
+		System.out.println(srsa.search(nums, target));
 	}
 	//33. 搜索旋转排序数组
     public int search(int[] nums, int target) {
+    	if((nums==null)||(nums.length==0)) {
+    		return -1;
+    	}
+    	//将数组分为[first,second]旋转后[second,first]
         int left = 0;
         int right = nums.length-1;
         while(left<=right) {
-        	
+        	int mid = left + (right-left)/2;
+        	if(nums[mid]==target) {
+        		return mid;
+        	}else if(nums[mid]<nums[right]) {
+        		//mid属于后部分
+        		//mid比target小，比right小，
+        		if((nums[mid]<target)&&(nums[right]>=target)) {
+        			//在右边
+        			left = mid +1;
+        		}else {
+        			//在左边
+        			right = mid -1;
+        		}
+        	}else {
+        		//mid属于前部分
+        		//left比target小，比mid比target大
+        		if((nums[left]<=target)&&(nums[mid]>target)) {
+        			//在左边
+        			right = mid -1;
+        		}else {
+        			//在右边
+        			left = mid + 1;
+        		}
+        	}
         }
+        return -1;
     }
 
 }
