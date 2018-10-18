e2cf056f82119f0f67a139da4104e9ca9484bf37
andrew <andrew@andrew-MS-7996>
2018-10-18 12:39:29 +0800

    26.删除排序数组中的重复项

--- ProblemSet/src/Leetcode_026_RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.java
+++ ProblemSet/src/Leetcode_026_RemoveDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.java
@@ -1,4 +1,4 @@
-package Leetcode_26_RemoveDuplicatesfromSortedArray;
+package Leetcode_026_RemoveDuplicatesfromSortedArray;
 
 /*
 
@@ -16,6 +16,7 @@ package Leetcode_26_RemoveDuplicatesfromSortedArray;
 		你不需要考虑数组中超出新长度后面的元素。
 */
 public class RemoveDuplicatesfromSortedArray {
+	//26.删除排序数组中的重复项
 	public int removeDuplicates(int[] nums) {
 		if (nums.length <= 1) {
 			return nums.length;
