b518032beb16de6f341806a41f40766cfb52d9c2
andrew <1124913956@qq.com>
2018-11-08 15:44:07 +0800

    66.加一

--- LG/Easy question/code by Java/PlusOne.java
+++ LG/Easy question/code by Java/PlusOne.java
@@ -1,5 +1,4 @@
 package Leetcode_066_PlusOne;
-
 /*
 
 给定一个非负整数组成的非空数组，在该数的基础上加一，返回一个新的数组。
@@ -17,22 +16,22 @@ package Leetcode_066_PlusOne;
 	解释: 输入数组表示数字 4321。
 */
 public class PlusOne {
-	// 66.加一
+	//66.加一
 	public int[] plusOne(int[] digits) {
-		// 获取数字的位数
+		//获取数字的位数
 		int length = digits.length;
-		digits[length - 1]++;
-		for (int i = length - 1; i >= 0; i--) {
-			if (digits[i] == 10) {
-				if (i == 0) {
-					int[] newdigits = new int[length + 1];
-					newdigits[0] = 1;
-					return newdigits;
-				}
-				digits[i] = 0;
-				digits[i - 1]++;
-			}
-		}
-		return digits;
+		digits[length-1]++;
+        for(int i = length-1;i>=0;i--) {
+        	if(digits[i]==10) {
+        		if(i==0) {
+        			int[] newdigits = new int[length+1];
+        			newdigits[0]=1;
+        			return newdigits;
+        		}
+        		digits[i]=0;
+        		digits[i-1]++;
+        	}
+        }
+        return digits;
 	}
 }
