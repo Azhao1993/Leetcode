c89b82962e52eb96cbd79fcbac0beae7b3026ff5
andrew <1124913956@qq.com>
2018-11-14 16:21:56 +0800

    14.最长公共前缀

--- ProblemSet/src/Leetcode_014_LongestCommonPrefix/LongestCommonPrefix.java
+++ ProblemSet/src/Leetcode_014_LongestCommonPrefix/LongestCommonPrefix.java
@@ -1,4 +1,5 @@
 package Leetcode_014_LongestCommonPrefix;
+
 /*
 	编写一个函数来查找字符串数组中的最长公共前缀。
 	
@@ -18,52 +19,52 @@ public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		String[] strs= {"babb","caa"};
+		String[] strs = { "babb", "caa" };
 		LongestCommonPrefix lcp = new LongestCommonPrefix();
 		System.out.println(lcp.longestCommonPrefix(strs));
 	}
-	//14.最长公共前缀
-    public String longestCommonPrefix(String[] strs) {
-    	if(strs.length==0) {
-    		return "";
-    	}else if(strs.length==1) {
-    		return strs[0];
-    	}
-    	//找出最短的单词
-        int minlength = strs[0].length();//最短单词的长度
-        int minindex = 0;//最短单词的位置
-        for(int i =1;i<strs.length;i++) {
-        	if(minlength>strs[i].length()) {
-        		minlength = strs[i].length();
-        		minindex = i;
-        	}
-        }
-        
-        //从最短单词开始比较 	
-        StringBuilder minbuffer = new StringBuilder(strs[minindex]);
-        //遍历所有单词
-        for(int i = 0;i<strs.length;i++) {           
-            int length = minbuffer.length();
-            //被删完，就返回
-            if(length==0) {
-            	return minbuffer.toString();
-            }
-            //判断strs[i]是否以minbuffer开头
-        	if(strs[i].startsWith(minbuffer.toString())) {
-        		//继续
-        		continue;
-        	}else {
-        		//否则，删除minbuffer的最后一个字符
-        		minbuffer.deleteCharAt(minbuffer.length()-1);
-        		i=-1;
-        		continue;
-        	}
-        	
-        }
-        
-        return minbuffer.toString();
-        
-        
-    }
+
+	// 14.最长公共前缀
+	public String longestCommonPrefix(String[] strs) {
+		if (strs.length == 0) {
+			return "";
+		} else if (strs.length == 1) {
+			return strs[0];
+		}
+		// 找出最短的单词
+		int minlength = strs[0].length();// 最短单词的长度
+		int minindex = 0;// 最短单词的位置
+		for (int i = 1; i < strs.length; i++) {
+			if (minlength > strs[i].length()) {
+				minlength = strs[i].length();
+				minindex = i;
+			}
+		}
+
+		// 从最短单词开始比较
+		StringBuilder minbuffer = new StringBuilder(strs[minindex]);
+		// 遍历所有单词
+		for (int i = 0; i < strs.length; i++) {
+			int length = minbuffer.length();
+			// 被删完，就返回
+			if (length == 0) {
+				return minbuffer.toString();
+			}
+			// 判断strs[i]是否以minbuffer开头
+			if (strs[i].startsWith(minbuffer.toString())) {
+				// 继续
+				continue;
+			} else {
+				// 否则，删除minbuffer的最后一个字符
+				minbuffer.deleteCharAt(minbuffer.length() - 1);
+				i = -1;
+				continue;
+			}
+
+		}
+
+		return minbuffer.toString();
+
+	}
 
 }
