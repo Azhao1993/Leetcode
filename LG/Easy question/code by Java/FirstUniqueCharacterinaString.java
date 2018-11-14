b853154d59299472b1d3764029f01ca96ed6f5c5
andrew <1124913956@qq.com>
2018-11-14 13:16:55 +0800

    387.字符串中的第一个唯一字符

--- LG/Easy question/code by Java/FirstUniqueCharacterinaString.java
+++ LG/Easy question/code by Java/FirstUniqueCharacterinaString.java
@@ -1,14 +1,14 @@
 package Leetcode_387_FirstUniqueCharacterinaString;
 
 /*
-	给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
-	
-	案例:	
-		s = "leetcode"
-		返回 0.
-	
-		s = "loveleetcode",
-		返回 2.
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+案例:	
+	s = "leetcode"
+	返回 0.
+
+	s = "loveleetcode",
+	返回 2.
 */
 public class FirstUniqueCharacterinaString {
 
