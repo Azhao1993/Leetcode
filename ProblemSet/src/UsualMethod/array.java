4253aa2915df38ecae8e59ff21bafdc76354f89d
Andrew <Andrew@118.202.42.146>
2018-11-16 17:45:56 +0800

    数组的swap操作

---
+++ ProblemSet/src/UsualMethod/array.java
@@ -0,0 +1,9 @@
+package UsualMethod;
+
+public class array {
+	public static void exchange(int[] nums,int i ,int j) {
+    	int temp = nums[i];
+    	nums[i] =nums[j];
+    	nums[j] = temp;
+    }
+}
