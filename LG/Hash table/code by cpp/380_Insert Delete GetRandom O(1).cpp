6487e49ad8365c250e4bea3f12d32f7914982bee
ontheway12138 <1358227862@qq.com>
2018-11-29 15:25:40 +0800

    380. 常数时间插入、删除和获取随机元素 hash_map记录位置 配合vector

--- LG/Hash table/code by cpp/380_Insert Delete GetRandom O(1).cpp
+++ LG/Hash table/code by cpp/380_Insert Delete GetRandom O(1).cpp
@@ -1,57 +1,76 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 /*
-706. 设计哈希映射
+380. 常数时间插入、删除和获取随机元素
 
-不使用任何内建的哈希表库设计一个哈希映射
+设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
 
-具体地说，你的设计应该包含以下的功能
+insert(val)：当元素 val 不存在时，向集合中插入该项。
+remove(val)：元素 val 存在时，从集合中移除该项。
+getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+示例 :
 
-put(key, value)：向哈希映射中插入(键,值)的数值对。如果键对应的值已经存在，更新这个值。
-get(key)：返回给定的键所对应的值，如果映射中不包含这个键，返回-1。
-remove(key)：如果映射中存在这个键，删除这个数值对。
+// 初始化一个空的集合。
+RandomizedSet randomSet = new RandomizedSet();
 
-示例：
+// 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+randomSet.insert(1);
 
-MyHashMap hashMap = new MyHashMap();
-hashMap.put(1, 1);
-hashMap.put(2, 2);
-hashMap.get(1);            // 返回 1
-hashMap.get(3);            // 返回 -1 (未找到)
-hashMap.put(2, 1);         // 更新已有的值
-hashMap.get(2);            // 返回 1
-hashMap.remove(2);         // 删除键为2的数据
-hashMap.get(2);            // 返回 -1 (未找到)
+// 返回 false ，表示集合中不存在 2 。
+randomSet.remove(2);
 
-注意：
+// 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+randomSet.insert(2);
 
-所有的值都在 [1, 1000000]的范围内。
-操作的总数目在[1, 10000]范围内。
-不要使用内建的哈希库。
+// getRandom 应随机返回 1 或 2 。
+randomSet.getRandom();
+
+// 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+randomSet.remove(1);
+
+// 2 已在集合中，所以返回 false 。
+randomSet.insert(2);
+
+// 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+randomSet.getRandom();
 */
 
 class RandomizedSet {
+private:
+    unordered_map<int, int> hash;
+    vector<int> arr;
 public:
     /** Initialize your data structure here. */
     RandomizedSet() {
-        
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        
+        if(hash.find(val) == hash.end()){
+            arr.push_back(val);
+            // 记录位置，方便删除
+            hash[val] = nums.size()-1;
+            return true;
+        }else return false;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        
+        if(hash.find(val) != hash.end()){
+            int last = arr.back();
+            // 改变没被删除的位置
+            hash[last] = hash[val];
+            nums[hash[val]] = last;
+            hash.erase(val);
+            arr.pop_back();
+            return true;
+        }else return false;
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-        
+        return arr[rand()%arr.size()];
     }
 };
 
@@ -61,41 +80,4 @@ public:
  * bool param_1 = obj.insert(val);
  * bool param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
- */
-
-
-
-
-
-class MyHashMap {
-private:
-    vector<int> arr;
-public:
-    /** Initialize your data structure here. */
-    MyHashMap() {
-        arr = vector<int>(1000001,-1);
-    }
-
-    /** value will always be non-negative. */
-    void put(int key, int value) {
-        arr[key]=value;
-    }
-
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    int get(int key) {
-        return arr[key];
-    }
-
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    void remove(int key) {
-        arr[key] = -1;
-    }
-};
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
  */
