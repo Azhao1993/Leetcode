48a235e7279ce8f9ddc4de6d712e2eed92bcd881
ontheway12138 <1358227862@qq.com>
2018-12-03 23:09:48 +0800

    101. 对称二叉树 两个栈迭代 递归

--- LG/Binary tree/code by cpp/101_Symmetric Tree.cpp
+++ LG/Binary tree/code by cpp/101_Symmetric Tree.cpp
@@ -25,16 +25,20 @@ using namespace std;
 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
  */
 
-// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-  };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 
 class Solution {
 public:
+    /*
+    // 递归
     bool isSymmetric(TreeNode* root) {
         if(!root)return true;
         else return Symmetric(root->left,root->right);
@@ -45,5 +49,31 @@ public:
         if(left->val == right->val)
             return Symmetric(left->right, right->left) && Symmetric(left->left, right->right);
         return false;
+    }
+    */
+    bool isSymmetric(TreeNode* root) {
+        if(!root)return true;
+        // 建立两个栈来判断
+        stack<TreeNode*> stal,star;
+        if(root->left)stal.push(root->left);
+        if(root->right)star.push(root->right);
+        if(stal.size()!=star.size())return false;
+
+        while(!stal.empty()){
+            TreeNode *teml = stal.top(),*temr = star.top();
+            if(teml->val != temr->val)return false;
+            stal.pop();star.pop();
+
+            if(teml->left && temr->right){
+                stal.push(teml->left);
+                star.push(temr->right);
+            }else if(teml->left || temr->right)return false;
+
+            if(teml->right && temr->left){
+                stal.push(teml->right);
+                star.push(temr->left);
+            }else if(teml->right || temr->left)return false;
+        }
+        return true;
     }
 };
