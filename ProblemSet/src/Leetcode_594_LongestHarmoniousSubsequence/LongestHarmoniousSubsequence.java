e9f8fe8a3c4110572d46594937a3953e9c9b2958
Andrew <Andrew@118.202.43.24>
2018-12-09 22:45:51 +0800

    594. 最长和谐子序列

--- ProblemSet/src/Leetcode_594_LongestHarmoniousSubsequence/LongestHarmoniousSubsequence.java
+++ ProblemSet/src/Leetcode_594_LongestHarmoniousSubsequence/LongestHarmoniousSubsequence.java
@@ -1,5 +1,6 @@
 package Leetcode_594_LongestHarmoniousSubsequence;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,7 +20,7 @@ public class LongestHarmoniousSubsequence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {1,3,2,2,5,2,3,7};
+		int[] nums = { 1, 3, 2, 2, 5, 2, 3, 7 };
 		System.out.println(new LongestHarmoniousSubsequence().findLHS(nums));
 	}
 
@@ -37,15 +38,44 @@ public class LongestHarmoniousSubsequence {
 			}
 			map.put(num, map.getOrDefault(num, 0) + 1);
 		}
-		int maxLength = Integer.MIN_VALUE;
-		for (int i = min; i <= max; i++) {
+		int maxLength = 0;
+		for (Integer i : map.keySet()) {
 			int count = 0;
-			if(map.containsKey(i+1)&&(map.containsKey(i))) {
-				count += map.get(i)+map.get(i+1);
+			if (map.containsKey(i + 1) && (map.containsKey(i))) {
+				count += map.get(i) + map.get(i + 1);
 				maxLength = Math.max(count, maxLength);
 			}
 		}
 		return maxLength;
+	}
+
+	// 31ms
+	public int findLHS0(int[] nums) {
+		if (nums == null || nums.length == 0 || nums.length == 1) {
+			return 0;
+		}
+		Arrays.sort(nums);
+		int max = 0, temp = nums[0], begin = 0, index = 0;
+		boolean isStart = true;
+		for (int i = 1; i <= nums.length; i++) {
+			if (i == nums.length || nums[i] != temp) {
+				if (!isStart) {
+					max = max > (i - begin) ? max : (i - begin);
+					begin = index;
+				}
+				if (i == nums.length)
+					break;
+				if (nums[i] - temp == 1) {
+					isStart = false;
+				} else {
+					isStart = true;
+					begin = i;
+				}
+				temp = nums[i];
+				index = i;
+			}
+		}
+		return max;
 	}
 
 }
