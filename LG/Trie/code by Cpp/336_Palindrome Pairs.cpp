6e1cb180a76a0549343b8a4e6068291b59df1930
ontheway12138 <1358227862@qq.com>
2018-12-10 21:58:25 +0800

    336. 回文对 前缀树+hash map

--- LG/Trie/code by Cpp/336_Palindrome Pairs.cpp
+++ LG/Trie/code by Cpp/336_Palindrome Pairs.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<set>
+#include<unordered_map>
 using namespace std;
 /*
 336. 回文对
@@ -21,73 +21,96 @@ using namespace std;
 
 class Solution {
 public:
-    // 前缀树的数据结构
+    bool isPalindrome(string s,int st,int end){
+        while(st < end){
+            if(s[st++] != s[end--])
+                return false;
+        }
+        return true;
+    }
     struct Trie{
-        vector<Trie*> child;
-        // 初始化
-        Trie() : child(vector<Trie*>(2,NULL)) {};
+        int index;
+        vector<int> list;
+        Trie *child[26];
+        Trie(): index(-1){
+            for(auto &it:child)it = NULL;
+        }
     };
-    void add(int num){
-        Trie* tmp = root;
-        for(int i=31; i>=0; i--){
-            int val = ((1<<i) & num) ? 1:0;
-            if(!tmp->child[val])tmp->child[val] = new Trie();
-            tmp = tmp->child[val];
+    void add(string word, int index){
+        Trie *tmp = root;
+        for(int i=word.length()-1;i>=0;i--){
+            int j = word[i]-'a';
+            if(!tmp->child[j])tmp->child[j] = new Trie();
+            // 若后面的为回文，则加到当前列表里，避免重复判断
+            if(isPalindrome(word,0,i))tmp->list.push_back(index);
+            tmp = tmp->child[j];
         }
+        tmp->list.push_back(index);
+        tmp->index = index;
     }
-    int search(int num){
-        int ans = 0;
+    void search(string word, int i, vector<vector<int> >& arr){
         Trie* tmp = root;
-        for(int i=31; i>=0; i--){
-            // 寻找的是异或，所以1 0对调
-            int val = ((1<<i) & num) ? 0:1;
-            if(tmp->child[val]){
-                // 更新ans的值
-                ans |= (1<<i);
-                tmp = tmp->child[val];
-            }else tmp = tmp->child[!val];
+        for(int j = 0;j<word.length();j++){
+            if(tmp->index>=0 && tmp->index!=i &&isPalindrome(word,j,word.length()-1))
+                arr.push_back(vector<int>{i,tmp->index});
+            tmp = tmp->child[word[j]-'a'];
+            if(!tmp)return;
         }
-        return ans;
+        // 匹配到最后找预存的列表
+        for(int j:tmp->list){
+            if(i==j)continue;
+            arr.push_back(vector<int>{i,j});
+        }
+
     }
-    int findMaximumXOR(vector<int>& nums) {
-        if(nums.size()<2)return 0;
+    vector<vector<int> > palindromePairs(vector<string>& words) {
+        vector<vector<int> > arr;
+        if(words.size()<2)return arr;
         root = new Trie();
-        for(int x:nums)add(x);
-        int maxNum = 0;
-        // 寻找最大值
-        for( int x:nums)maxNum = max(maxNum,search(x));
-        return maxNum;
-        /*
-        if(nums.size()<2)return 0;
-        int maxNum = 0;
-        int flag = 0;
-        for(int i=31; i>=0; i--){
-            set<int> hash;
-            flag |= (1<<i);
-            for(int x:nums)hash.insert(flag&x);
-            // tmp为有可能的最大值，在前缀数中找到时，即为最大值
-            int tmp = maxNum | (1<<i);
-            for(int prefix:hash)
-                // 此处用到了x1^tmp=x2,则x1^x2=tmp, 两个前缀若都存在，则更新最大值
-                if(hash.find(prefix ^ tmp) != hash.end()){
-                    maxNum = tmp;
-                    break;
-                }
-        }
-        return maxNum;
-        */
+        for(int i=0;i<words.size();i++)add(words[i],i);
+        for(int i=0;i<words.size();i++)search(words[i],i,arr);
+        return arr;
     }
 private:
     Trie* root;
+    /*
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        while(left < right){
+            if(s[left++] != s[right--])
+                return false;
+        }
+        return true;
+    }
+    vector<vector<int> > palindromePairs(vector<string>& words) {
+        unordered_map<string, int>hash;
+        vector<vector<int> > arr;
+        // 正序存hash，倒序寻找
+        for(int i = 0; i<words.size();i++)hash[words[i]]=i;
+        for(int i = 0; i<words.size();i++){
+            reverse(words[i].begin(),words[i].end());
+            int size = words[i].length();
+            for(int j = 0; j<=size;j++){
+                // 分成左右两半进行查找
+                string left=words[i].substr(0,j),right=words[i].substr(j);
+                if(hash.count(left) && isPalindrome(right) && hash[left]!=i)
+                    arr.push_back(vector<int>{hash[left],i});
+                // 不加j!=0，会重复判断
+                if(hash.count(right) && isPalindrome(left) && hash[right]!=i && j!=0)
+                    arr.push_back(vector<int>{i,hash[right]});
+            }
+        }
+        return arr;
+    }
+    */
 };
 
-
-
 int main(){
-    int x[6] = {3, 10, 5, 25, 2, 8};
-    vector<int>nums(x,x+6);
+    string x[5] = {"abcd","dcba","lls","s","sssll"};
+    vector<string>nums(x,x+5);
     auto so = new Solution();
-    int n = so->findMaximumXOR(nums);
-    cout<<n<<endl;
+    vector<vector<int> > n = so->palindromePairs(nums);
+    for(auto it:n)
+        cout<<it[0]<<' '<<it[1]<<endl;
     return 0;
 }
