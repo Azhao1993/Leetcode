25225e2d715774e14d4ab3d1fa7aeab9b3df31c0
ontheway12138 <1358227862@qq.com>
2018-12-13 19:15:44 +0800

    9. 回文数 一位一位反置

---
+++ LG/Tencent/code by cpp/009_Palindrome Number.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/*
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+示例 1:
+
+输入: 121
+输出: true
+示例 2:
+
+输入: -121
+输出: false
+解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3:
+
+输入: 10
+输出: false
+解释: 从右向左读, 为 01 。因此它不是一个回文数。
+进阶:
+
+你能不将整数转为字符串来解决这个问题吗？
+
+*/
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        if(x<10)return true;
+        int temp = x,num = 0;
+        while(temp){
+            num = num*10 + temp%10;
+            temp /= 10;
+        }
+        return x==num;
+    }
+};
+
+int main(){
+
+    auto so = new Solution();
+    bool num = so->isPalindrome(787);
+
+    cout<<num<<endl;
+
+	return 0;
+}
