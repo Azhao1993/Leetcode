fbff11f5263384769cbeca320ae2a09ad678b880
andrew <1124913956@qq.com>
2018-12-14 19:48:19 +0800

    链表节点

---
+++ ProblemSet/src/LinkedList/ListNode.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+/**
+ *
+ * Definition for singly-linked list.
+ *
+ */
+//Á´±í½Úµã
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}	
+	
+}
