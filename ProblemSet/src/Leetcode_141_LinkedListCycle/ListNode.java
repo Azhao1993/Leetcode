0d964058d0dd03dba20f59d5cdb7944362c3fa39
andrew <1124913956@qq.com>
2018-12-14 11:43:26 +0800

    141. 环形链表

---
+++ ProblemSet/src/Leetcode_141_LinkedListCycle/ListNode.java
@@ -0,0 +1,16 @@
+package Leetcode_141_LinkedListCycle;
+
+/**
+ *
+ * Definition for singly-linked list.
+ *
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}	
+	
+}
