44c98081d98bae3c3662f54fb213e8ad8ddd9956
andrew <1124913956@qq.com>
2018-12-20 12:18:25 +0800

    Definition for singly-linked list with a random pointer.

---
+++ ProblemSet/src/Leetcode_138_CopyListwithRandomPointer/RandomListNode.java
@@ -0,0 +1,14 @@
+package Leetcode_138_CopyListwithRandomPointer;
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * 
+ */
+public class RandomListNode {
+	int label;
+	RandomListNode next, random;
+
+	RandomListNode(int x) {
+		this.label = x;
+	}
+}
