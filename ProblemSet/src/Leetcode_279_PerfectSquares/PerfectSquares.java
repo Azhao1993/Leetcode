c7a27c646ed9ac4df39ead4aebf8b9bdcc4e0846
andrew <1124913956@qq.com>
2018-12-29 22:44:54 +0800

    279. 完全平方数

---
+++ ProblemSet/src/Leetcode_279_PerfectSquares/PerfectSquares.java
@@ -0,0 +1,31 @@
+package Leetcode_279_PerfectSquares;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+	给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。
+	你需要让组成和的完全平方数的个数最少。
+	
+	示例 1:	
+		输入: n = 12
+		输出: 3 
+		解释: 12 = 4 + 4 + 4.
+	
+	示例 2:	
+		输入: n = 13
+		输出: 2
+		解释: 13 = 4 + 9.
+ */
+public class PerfectSquares {
+	public static void main(String[] args) {
+		int n = 12;
+		PerfectSquares ps = new PerfectSquares();
+		System.out.println(ps.numSquares(n));
+	}
+
+	// 279. 完全平方数
+	public int numSquares(int n) {
+		
+	}
+}
