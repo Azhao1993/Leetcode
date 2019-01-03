4c01ea6b1671ba4288e6009e85d1244227aa4f74
andrew <1124913956@qq.com>
2019-01-03 09:32:36 +0800

    BinaryTree

--- ProblemSet/src/TreeNode/TreeNode.java
+++ ProblemSet/src/TreeNode/TreeNode.java
@@ -4,11 +4,11 @@ package TreeNode;
  * Definition for a binary tree node.
  */
 public class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
 
-	TreeNode(int x) {
+	public TreeNode(int x) {
 		val = x;
 	}
 }
