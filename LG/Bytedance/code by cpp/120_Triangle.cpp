ae45a717f0600738ab3427571873c56bff9b8c94
ontheway12138 <1358227862@qq.com>
2019-01-07 09:08:01 +0800

    120. 三角形最小路径和 自底向上+dp数组循环使用

--- LG/Bytedance/code by cpp/120_Triangle.cpp
+++ LG/Bytedance/code by cpp/120_Triangle.cpp
@@ -15,6 +15,8 @@ using namespace std;
   [4,1,8,3]
 ]
 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+说明：
+如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。
  */
 
 class Solution {
@@ -61,7 +63,6 @@ public:
             res = min(res,dp[i]);
         return res;
         */
-
     }
 };
 
