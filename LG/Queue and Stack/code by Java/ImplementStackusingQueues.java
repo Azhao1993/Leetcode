d9d0cce8cec124daf770d23df336f3b82aa783aa
andrew <1124913956@qq.com>
2019-01-09 16:28:43 +0800

    225. 用队列实现栈

--- LG/Queue and Stack/code by Java/ImplementStackusingQueues.java
+++ LG/Queue and Stack/code by Java/ImplementStackusingQueues.java
@@ -24,7 +24,7 @@ class ImplementStackusingQueues {
 	public int peek;
 
 	/** Initialize your data structure here. */
-	public MyStack() {
+	public ImplementStackusingQueues() {
 		queue = new LinkedList<Integer>();
 		queue2 = new LinkedList<Integer>();
 	}
@@ -67,10 +67,7 @@ class ImplementStackusingQueues {
 	}
 }
 /**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
+ * Your MyStack object will be instantiated and called as such: MyStack obj =
+ * new MyStack(); obj.push(x); int param_2 = obj.pop(); int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
