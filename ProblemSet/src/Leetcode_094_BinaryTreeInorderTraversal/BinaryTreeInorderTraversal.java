046daea192435ee7440b3400819f6a036471535b
andrew <1124913956@qq.com>
2019-01-15 22:52:22 +0800

    94. 二叉树的中序遍历(迭代和遍历)

--- ProblemSet/src/Leetcode_094_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.java
+++ ProblemSet/src/Leetcode_094_BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.java
@@ -2,6 +2,7 @@ package Leetcode_094_BinaryTreeInorderTraversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import TreeNode.TreeNode;
 
@@ -40,7 +41,26 @@ public class BinaryTreeInorderTraversal {
 		list.add(root.val);
 		loop(root.right);
 	}
-
+	//µü´ú
+	public List<Integer> inorderTraversal2(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		if(root==null) {
+			return result;
+		}
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		while(!stack.isEmpty()) {
+			TreeNode cur = stack.peek();
+			result.add(cur.val);
+			if(cur.right!=null) {
+				stack.push(cur.right);
+			}
+			
+			if(cur.left!=null) {
+				stack.push(cur.left);
+			}
+		}
+	}
 	// 0ms
 	public List<Integer> inorderTraversal0(TreeNode root) {
 		List<Integer> result = new ArrayList<>();
