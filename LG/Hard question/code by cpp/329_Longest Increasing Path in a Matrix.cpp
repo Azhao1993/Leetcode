3deb39a8f474509c323012562e6b468d91303724
ontheway12138 <1358227862@qq.com>
2019-01-18 10:21:16 +0800

    329. 矩阵中的最长递增路径 dp矩阵

--- LG/Hard question/code by cpp/329_Longest Increasing Path in a Matrix.cpp
+++ LG/Hard question/code by cpp/329_Longest Increasing Path in a Matrix.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<limits.h>
 #include<queue>
 using namespace std;
 /*
@@ -9,22 +10,22 @@ using namespace std;
 对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
 
 示例 1:
-输入: nums = 
+输入: nums =
 [
   [9,9,4],
   [6,6,8],
   [2,1,1]
-] 
-输出: 4 
+]
+输出: 4
 解释: 最长递增路径为 [1, 2, 6, 9]。
 示例 2:
-输入: nums = 
+输入: nums =
 [
   [3,4,5],
   [3,2,6],
   [2,2,1]
-] 
-输出: 4 
+]
+输出: 4
 解释: 最长递增路径是 [3, 4, 5, 6]。注意不允许在对角线方向上移动。
 */
 
@@ -36,19 +37,18 @@ public:
         vector<vector<int> > dp(matrix.size(),vector<int>(matrix[0].size(),0));
         for(int i=0;i<matrix.size();i++)
             for(int j=0;j<matrix[0].size();j++)
-                res = max(res,helper(matrix,i,j,dp));
+                res = max(res,helper(matrix,INT_MIN,i,j,dp));
         return res;
     }
-    int helper(vector<vector<int> > matrix,int i,int j,vector<vector<int> >&dp){
+    int helper(vector<vector<int> > matrix,int cur,int i,int j,vector<vector<int> >&dp){
+        if(i<0 || j<0 || i>=matrix.size() || j>=matrix[0].size() || matrix[i][j]<=cur)
+            return 0;
         if(dp[i][j])return dp[i][j];
-        dp[i][j] = 1;
-        int dx[]={-1,1,0,0};
-        int dy[]={0,0,-1,1};
-        for(int k=0;k<4;k++){
-            int x = i+dx[k],y = j+dy[k];
-            if(x>=0 && y>=0 && x<matrix.size() && y<matrix[0].size() && matrix[x][y]>matrix[i][j])
-                dp[i][j] = max(dp[i][j],1+helper(matrix,x,y,dp));
-        }
+        int l = helper(matrix,matrix[i][j],i-1,j,dp);
+        int r = helper(matrix,matrix[i][j],i+1,j,dp);
+        int u = helper(matrix,matrix[i][j],i,j-1,dp);
+        int d = helper(matrix,matrix[i][j],i,j+1,dp);
+        dp[i][j] = max(l,max(r,max(u,d)))+1;
         return dp[i][j];
     }
 };
