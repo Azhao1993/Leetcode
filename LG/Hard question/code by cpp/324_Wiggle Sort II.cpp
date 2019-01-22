6dc171116a15476d1ab56e7407db955db779dceb
ontheway12138 <1358227862@qq.com>
2019-01-22 17:58:13 +0800

    324. 摆动排序 II 简单易写

--- LG/Hard question/code by cpp/324_Wiggle Sort II.cpp
+++ LG/Hard question/code by cpp/324_Wiggle Sort II.cpp
@@ -23,6 +23,7 @@ public:
     void wiggleSort(vector<int>& nums) {
         vector<int> sorted(nums);
         sort(sorted.begin(), sorted.end());
+        // 双指针，j为前半部分  奇数， k为后半部分   偶数
         for (int i=nums.size()-1, j=0, k=i/2+1; i>=0; i--)
             nums[i] = sorted[i&1 ? k++ : j++];
     }
