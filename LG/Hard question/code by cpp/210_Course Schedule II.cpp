31a37ada91cfa2cc38348250f8f1ca41f4428abf
ontheway12138 <1358227862@qq.com>
2019-01-27 18:36:39 +0800

    210. 课程表 II 和1一样 DFS+邻接矩阵

---
+++ LG/Hard question/code by cpp/210_Course Schedule II.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+/*
+210. 课程表 II
+
+现在你总共有 n 门课需要选，记为 0 到 n-1。
+在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们: [0,1]
+给定课程总量以及它们的先决条件，返回你为了学完所有课程所安排的学习顺序。
+可能会有多个正确的顺序，你只要返回一种就可以了。如果不可能完成所有课程，返回一个空数组。
+示例 1:
+输入: 2, [[1,0]] 
+输出: [0,1]
+解释: 总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+示例 2:
+输入: 4, [[1,0],[2,0],[3,1],[3,2]]
+输出: [0,1,2,3] or [0,2,1,3]
+解释: 总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+     因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+说明:
+输入的先决条件是由边缘列表表示的图形，而不是邻接矩阵。详情请参见图的表示法。
+你可以假定输入的先决条件中没有重复的边。
+提示:
+这个问题相当于查找一个循环是否存在于有向图中。如果存在循环，则不存在拓扑排序，因此不可能选取所有课程进行学习。
+通过 DFS 进行拓扑排序 - 一个关于Coursera的精彩视频教程（21分钟），介绍拓扑排序的基本概念。
+拓扑排序也可以通过 BFS 完成。
+*/
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        // 若入度为0，则可以选修这门课
+        vector<int> inDegree(numCourses,0);
+        vector<int> adjecent[numCourses];
+        queue<int> que;
+        for(auto x:prerequisites){
+            // 在前面的话，入度+1，后面的指向前面的。
+            adjecent[x.second].push_back(x.first);
+            inDegree[x.first]++;
+        }
+        // 把当前可以选修的入队列
+        for(int i=0;i<inDegree.size();i++)
+            if(!inDegree[i])que.push(i);
+        vector<int> res;
+        while(!que.empty()){
+            int v = que.front();
+            que.pop();
+            res.push_back(v);
+            // 把先修课程为v的课程度数-1，来判断是否能修其他课程
+            for(auto it:adjecent[v])
+                if(!--inDegree[it])que.push(it);  
+        }
+        return res.size()==numCourses ? res:vector<int> {};
+    }
+};
+
+
+int main(){
+    vector<pair<int, int> > a({{1,0},{0,1},{3,1},{3,2}});
+
+    Solution* so = new Solution();
+    vector<int> arr = so->findOrder(4,a);
+    for(auto n:arr)
+        cout<<n<<' ';
+    return 0;
+}
