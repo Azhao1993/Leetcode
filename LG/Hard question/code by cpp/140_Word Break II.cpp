b58a324dca6eb682ccd7375aa19b579d8c9691bd
ontheway12138 <1358227862@qq.com>
2019-02-13 21:17:14 +0800

    待修改

--- LG/Hard question/code by cpp/140_Word Break II.cpp
+++ LG/Hard question/code by cpp/140_Word Break II.cpp
@@ -11,31 +11,15 @@ using namespace std;
 分隔时可以重复使用字典中的单词。
 你可以假设字典中没有重复的单词。
 示例 1：
-输入:
-s = "catsanddog"
-wordDict = ["cat", "cats", "and", "sand", "dog"]
-输出:
-[
-  "cats and dog",
-  "cat sand dog"
-]
+输入: s = "catsanddog"    wordDict = ["cat", "cats", "and", "sand", "dog"]
+输出: ["cats and dog","cat sand dog"]
 示例 2：
-输入:
-s = "pineapplepenapple"
-wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
-输出:
-[
-  "pine apple pen apple",
-  "pineapple pen apple",
-  "pine applepen apple"
-]
+输入: s = "pineapplepenapple"     wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+输出: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
 解释: 注意你可以重复使用字典中的单词。
 示例 3：
-输入:
-s = "catsandog"
-wordDict = ["cats", "dog", "sand", "and", "cat"]
-输出:
-[]
+输入: s = "catsandog"  wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: []
 */
 
 class Solution {
