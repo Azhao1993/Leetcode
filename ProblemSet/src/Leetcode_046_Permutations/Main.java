1a7870afc347cf8be9e96d5da4f2214becc24bcb
Andrew <Andrew@118.202.43.24>
2019-02-21 19:28:06 +0800

    46. 全排列

---
+++ ProblemSet/src/Leetcode_046_Permutations/Main.java
@@ -0,0 +1,9 @@
+package Leetcode_046_Permutations;
+
+public class Main {
+	public static void main(String[] args) {
+		Permutations p = new Permutations();
+		int[] nums = {1,2,3};
+		p.permute(nums);		
+	}
+}
