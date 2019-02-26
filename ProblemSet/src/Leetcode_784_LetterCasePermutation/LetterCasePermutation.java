bd9a97fdeecfe66242c55c94259dbb7a8561ad38
andrew <1124913956@qq.com>
2019-02-26 17:17:14 +0800

    784. 字母大小写全排列

--- ProblemSet/src/Leetcode_784_LetterCasePermutation/LetterCasePermutation.java
+++ ProblemSet/src/Leetcode_784_LetterCasePermutation/LetterCasePermutation.java
@@ -1,4 +1,8 @@
 package Leetcode_784_LetterCasePermutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 	给定一个字符串S，通过将字符串S中的每个字母转变大小写，我们可以获得一个新的字符串。返回所有可能得到的字符串集合。
 	
@@ -20,8 +24,62 @@ public class LetterCasePermutation {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
+		String S = "a1b2";
+		LetterCasePermutation lcp = new LetterCasePermutation();
+		lcp.letterCasePermutation(S);
+	}
+
+	// 784. 字母大小写全排列
+	public List<String> letterCasePermutation(String S) {
+		List<String> result = new ArrayList<String>();
+		StringBuilder temStr = new StringBuilder(S);
+		letterCasePermutation(temStr, S, S.length() - 1, result);
+
+		return result;
+	}
+
+	public void letterCasePermutation(StringBuilder temStr, String S, int end, List<String> result) {
+		result.add(temStr.toString());
+		for (int i = end; i >= 0; i--) {
+			char temp = S.charAt(i);
+			if (temp >= 'a' && temp <= 'z') {
+				temStr.replace(i, i + 1, String.valueOf((char) (temp - 32)));
+				letterCasePermutation(temStr, S, i - 1, result);
+				temStr.replace(i, i + 1, String.valueOf(temp));
+			} else if (temp >= 'A' && temp <= 'Z') {
+				temStr.replace(i, i + 1, String.valueOf((char) (temp + 32)));
+				letterCasePermutation(temStr, S, i - 1, result);
+				temStr.replace(i, i + 1, String.valueOf(temp));
+
+			}
+		}
+	}
+
+	// 4ms
+	public List<String> letterCasePermutation0(String S) {
+		List<String> list = new ArrayList<>();
+		if (S == null)
+			return list;
+		dfs(S.toCharArray(), 0, list);
+		return list;
+	}
+
+	void dfs(char[] chars, int index, List<String> list) {
+		if (index == chars.length) {
+			list.add(new String(chars));
+			return;
+		}
+		char c = chars[index];
+		dfs(chars, index + 1, list);
+		if (chars[index] >= 'a') {
+			chars[index] = (char) (chars[index] - 32);
+			dfs(chars, index + 1, list);
+		} else if (chars[index] >= 'A') {
+			chars[index] = (char) (chars[index] + 32);
+			dfs(chars, index + 1, list);
+		}
+
+		chars[index] = c;
 
 	}
-	//784. 字母大小写全排列
-
 }
