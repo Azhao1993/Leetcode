5f77edea0f428e464456ca18ca631b21b632c662
andrew <1124913956@qq.com>
2019-03-01 17:17:42 +0800

    62. 不同的路径

--- ProblemSet/src/Leetcode_062_UniquePaths/UniquePaths.java
+++ ProblemSet/src/Leetcode_062_UniquePaths/UniquePaths.java
@@ -51,4 +51,16 @@ public class UniquePaths {
 		return grid[m - 1][n - 1];
 	}
 
+	// 0ms:Ò»Î¬dp¾ØÕó
+	public int uniquePaths0(int m, int n) {
+		int[] res = new int[n];
+		res[0] = 1;
+		for (int i = 0; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				res[j] = res[j] + res[j - 1];
+			}
+		}
+		return res[n - 1];
+	}
+
 }
