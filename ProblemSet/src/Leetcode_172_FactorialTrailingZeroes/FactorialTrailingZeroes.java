e48fcbefda6a677fdfbeced7f23f8be8504aa5ce
Andrew <Andrew@118.202.43.24>
2019-03-03 16:50:57 +0800

    172.阶乘后的零(只需要计算包含多少个5)

--- ProblemSet/src/Leetcode_172_FactorialTrailingZeroes/FactorialTrailingZeroes.java
+++ ProblemSet/src/Leetcode_172_FactorialTrailingZeroes/FactorialTrailingZeroes.java
@@ -13,9 +13,36 @@ package Leetcode_172_FactorialTrailingZeroes;
 	说明: 你算法的时间复杂度应为 O(log n) 。
  */
 
-//172.阶乘后的零
+//172.阶乘后的零(只需要计算包含多少个5)
 public class FactorialTrailingZeroes {
-    public int trailingZeroes(int n) {
-        
-    }
+	public static void main(String[] args) {
+		System.out.println(new FactorialTrailingZeroes().trailingZeroes(2147483647));
+	}
+
+	// 
+	public int trailingZeroes(int n) {
+		// int countoftwo = 0;
+		int countoffive = 0;
+		while (n >= 5) {
+			if (n % 5 != 0) {
+				n--;
+				continue;
+			}
+			int temp5 = n;
+			while (temp5 % 5 == 0) {
+				countoffive++;
+				temp5 /= 5;
+			}
+			n -= 5;
+		}
+		return countoffive;
+	}
+
+	// 13ms
+	public int trailingZeroes0(int n) {
+		if (n < 5)
+			return 0;
+		else
+			return n / 5 + trailingZeroes(n / 5);
+	}
 }
