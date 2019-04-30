14d39afd34e1bed797eebf04c47fc037418666ad
ontheway12138 <1358227862@qq.com>
2019-04-30 15:51:30 +0800

    5039. 移动石子直到连续 先排序再判断

--- AnswerByCpp/5039_Moving Stones Until Consecutive.cpp
+++ AnswerByCpp/5039_Moving Stones Until Consecutive.cpp
@@ -1,26 +1,21 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
-#include<numeric>
+#include<algorithm>
 using namespace std;
 /*
 5039. 移动石子直到连续
 
 三枚石子放置在数轴上，位置分别为 a，b，c。
 每一回合，我们假设这三枚石子当前分别位于位置 x, y, z 且 x < y < z。
-从位置 x 或者是位置 z 拿起一枚石子，并将该石子移动到某一整数位置 k 处，
-其中 x < k < z 且 k != y。
+从位置 x 或者是位置 z 拿起一枚石子，并将该石子移动到某一整数位置 k 处，其中 x < k < z 且 k != y。
 当你无法进行任何移动时，即，这些石子的位置连续时，游戏结束。
 当游戏结束时，你可以执行的最小和最大移动次数分别是多少？ 
 以长度为 2 的数组形式返回：answer = [minimum_moves, maximum_moves]
 
-示例 1：
-输入：a = 1, b = 2, c = 5
-输出：[1, 2]
+示例 1：   输入：a = 1, b = 2, c = 5      输出：[1, 2]
 解释：将石子从 5 移动到 4 再移动到 3，或者我们可以直接将石子移动到 3。
-示例 2：
-输入：a = 4, b = 3, c = 2
-输出：[0, 0]
+示例 2：   输入：a = 4, b = 3, c = 2      输出：[0, 0]
 解释：我们无法进行任何移动。
 
 提示
@@ -33,23 +28,22 @@ a != b, b != c, c != a
 class Solution {
 public:
     vector<int> numMovesStones(int a, int b, int c) {
-        int maxN = max(a,max(b,c));
-        int minN = min(a,min(b,c));
-        int midN = a+b+c-maxN-minN;
+        vector<int> arr = {a, b, c};
+        sort(arr.begin(), arr.end());
 
-        int min_moves = min(1,maxN-midN-1) + min(1, midN-minN-1);
-        int max_moves = maxN-midN-1 + midN-minN-1;
-        
-        vector<int> arr;
-        arr.push_back(min_moves);
-        arr.push_back(max_moves);
-        return arr;
+        int min_moves = 2, max_moves = arr[2]-arr[0]-2;
+        // 不需移动
+        if(arr[2] == arr[0]+2) min_moves = 0;
+        // 能够一步到位的情况
+        else if(arr[1] <= arr[0]+2 || arr[2] <= arr[1]+2) min_moves = 1;
+
+        return {min_moves, max_moves};
     }
 };
 
 int main(){
     Solution* so = new Solution();
-    bool bl = so->divisorGame(6);
-    cout<<bl<<endl;
+    vector<int> arr = so->numMovesStones(1,4,67);
+    cout<<arr[0]<<"  "<<arr[1]<<endl;
     return 0;
 }
