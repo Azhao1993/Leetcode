1574c31dcc29bd3e29e3154aae2c82f178070a2c
ontheway12138 <1358227862@qq.com>
2019-05-07 09:58:53 +0800

    1027. 最长等差数列  hashmap存放下标进行二分查找 + hashset暴力查找 + 动态规划

--- AnswerByCpp/1027_Longest Arithmetic Sequence.cpp
+++ AnswerByCpp/1027_Longest Arithmetic Sequence.cpp
@@ -6,7 +6,7 @@
 #include<algorithm>
 using namespace std;
 /*
-5025. 最长等差数列
+1027. 最长等差数列
 
 给定一个整数数组 A，返回 A 中最长等差子序列的长度。
 回想一下，A 的子序列是列表 A[i_1], A[i_2], ..., A[i_k] 其中 0 <= i_1 < i_2 < ... < i_k <= A.length - 1。并且如果 B[i+1] - B[i]( 0 <= i < B.length - 1) 的值都相同，那么序列 B 是等差的。
