1131801e9e98d69ad4fb1f46d85a3c07967a761c
ontheway12138 <1358227862@qq.com>
2019-06-17 18:54:22 +0800

    1058. 最小化舍入误差   把整数和小数部分单独处理

---
+++ AnswerByCpp/1058_Minimize Rounding Error to Meet Target.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+/*
+1058. 最小化舍入误差
+
+给定一系列价格 [p1,p2...,pn] 和一个目标 target，将每个价格 pi 舍入为 Roundi(pi) 以使得舍入数组 
+[Round1(p1),Round2(p2)...,Roundn(pn)] 之和达到给定的目标值 target。每次舍入操作 Roundi(pi) 
+可以是向下舍 Floor(pi) 也可以是向上入 Ceil(pi)。如果舍入数组之和无论如何都无法达到目标值 target，
+就返回 -1。否则，以保留到小数点后三位的字符串格式返回最小的舍入误差，
+其定义为 Σ |Roundi(pi) - (pi)|（ i 从 1 到 n ）。
+
+示例 1：   输入：prices = ["0.700","2.800","4.900"], target = 8   输出："1.000"
+解释： 使用 Floor，Ceil 和 Ceil 操作得到 (0.7-0) + (3-2.8) + (5-4.9) = 0.7+0.2+0.1 = 1.0 。
+示例 2：   输入：prices = ["1.500","2.500","3.500"], target = 10  输出："-1"
+解释： 达到目标是不可能的。
+
+提示： 1 <= prices.length <= 500
+表示价格的每个字符串 prices[i] 都代表一个介于 0 和 1000 之间的实数，并且正好有 3 个小数位。
+target 介于 0 和 1000000 之间。
+*/
+
+class Solution {
+public:
+    string minimizeError(vector<string>& prices, int target) {
+        int sum = 0;
+        priority_queue<int> error;
+        for(int i=0; i<prices.size(); ++i){
+            // 去掉小数点
+            int len = prices[i].length();
+            // 计算出可能得到的最小数
+            sum += atoi(prices[i].substr(0,len-4).c_str());
+            int tem = atoi(prices[i].substr(len-3).c_str());
+            // 当前数有小数
+            if(tem) error.push(tem);
+        }
+        // target 不在这些数的范围
+        if(sum > target || sum + error.size() < target) return "-1";
+
+        // 向上取整的个数
+        int count = target - sum;
+        int res = 0;
+        while(error.size()){
+            if(count-- > 0){
+                res += 1000 - error.top();
+                error.pop();
+            }else{
+                res += error.top();
+                error.pop();
+            }
+        }
+        char str[10];
+        sprintf(str, "%.3f", (double)res/1000);
+        return str;
+    }
+};
+
+int main(){
+    vector<string> str({"0.700","2.800","4.900"});
+
+    Solution* so = new Solution();
+    string res = so->minimizeError(str, 9);
+    cout<<res<<endl;
+    return 0;
+}
