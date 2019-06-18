2f2c57fae094c3c109da5ed7c98bcfdc73ed60e5
ontheway12138 <1358227862@qq.com>
2019-06-18 12:30:20 +0800

    1036. 逃离大迷宫  带限制的BFS  大佬级操作

--- AnswerByCpp/1036_Escape a Large Maze.cpp
+++ AnswerByCpp/1036_Escape a Large Maze.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
-#include<numeric>
+#include<unordered_set>
 using namespace std;
 /*
 1036. 逃离大迷宫
@@ -26,15 +25,46 @@ source != target
 */
 
 class Solution {
+private:
+	// 查找过的 hashmap  起点  终点   blocks 的大小
+	bool bfs(unordered_set<long long>& visited, vector<int>& s, vector<int>& t, int blocks){
+		vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+		vector<pair<int, int>> que = {{s[0], s[1]}};
+		while( !que.empty() && que.size() <= blocks) {
+			// 强者的 BFS 操作
+			vector<pair<int, int>> que1;
+			for(auto it:que)
+				for(auto d:dirs){
+					auto x = it.first + d[0], y = it.second + d[1];
+					if(x<0 || x>=1e6 || y<0 || y>=1e6) continue ;
+					if(x == t[0] && y == t[1]) return true;
+					// unordered_set 的 insert 函数返回值为 pair   
+					// first 为插入元素的指针  second  为是否插入成功的标志
+					if(visited.insert(((long long) x << 20) + y).second) 
+						que1.push_back({x, y});
+				}
+			swap(que, que1);
+		}
+		// 若不为空，则为提前终止，即为真
+		return !que.empty();
+	}
 public:
     bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
-        
+        unordered_set<long long> vis_s, vis_t;
+        // 2^20 = 1024*1024 > 999999
+        for(auto b:blocked){
+        	if(abs(b[0]-source[0]) + abs(b[1]-source[1]) < 400) vis_s.insert(((long long) b[0] << 20)+b[1]);
+        	if(abs(b[0]-target[0]) + abs(b[1]-target[1]) < 400) vis_t.insert(((long long) b[0] << 20)+b[1]);
+        }
+        return bfs(vis_s, source, target, (int)vis_s.size()) && bfs(vis_t, target, source, (int)vis_t.size());
     }
 };
 
 int main(){
-    Solution* so = new Solution();
-    bool bl = so->divisorGame(6);
+    vector<vector<int>> block{{0, 1}, {1, 1}};
+    vector<int> source{0, 0};
+    vector<int> target{0, 2};
+    bool bl = Solution().isEscapePossible(block, source, target);
     cout<<bl<<endl;
     return 0;
 }
