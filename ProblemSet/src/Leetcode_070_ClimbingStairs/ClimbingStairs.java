c2020da1ee98369943f1334c7a2bfc5467151ba4
Andrew <Andrew@Andrew-pc>
2019-06-19 11:45:53 +0800

    70. 爬楼梯（6种方法）

--- ProblemSet/src/Leetcode_070_ClimbingStairs/ClimbingStairs.java
+++ ProblemSet/src/Leetcode_070_ClimbingStairs/ClimbingStairs.java
@@ -3,62 +3,157 @@ package Leetcode_070_ClimbingStairs;
 import java.util.HashMap;
 
 /*
-	¼ÙÉèÄãÕıÔÚÅÀÂ¥Ìİ¡£ĞèÒª n ½×Äã²ÅÄÜµ½´ïÂ¥¶¥¡£	
-	Ã¿´ÎÄã¿ÉÒÔÅÀ 1 »ò 2 ¸öÌ¨½×¡£ÄãÓĞ¶àÉÙÖÖ²»Í¬µÄ·½·¨¿ÉÒÔÅÀµ½Â¥¶¥ÄØ£¿
+	å‡è®¾ä½ æ­£åœ¨çˆ¬æ¥¼æ¢¯ã€‚éœ€è¦ n é˜¶ä½ æ‰èƒ½åˆ°è¾¾æ¥¼é¡¶ã€‚	
+	æ¯æ¬¡ä½ å¯ä»¥çˆ¬ 1 æˆ– 2 ä¸ªå°é˜¶ã€‚ä½ æœ‰å¤šå°‘ç§ä¸åŒçš„æ–¹æ³•å¯ä»¥çˆ¬åˆ°æ¥¼é¡¶å‘¢ï¼Ÿ
 		
-	×¢Òâ£º¸ø¶¨ n ÊÇÒ»¸öÕıÕûÊı¡£
+	æ³¨æ„ï¼šç»™å®š n æ˜¯ä¸€ä¸ªæ­£æ•´æ•°ã€‚
 	
-	Ê¾Àı 1£º	
-		ÊäÈë£º 2
-		Êä³ö£º 2
-		½âÊÍ£º ÓĞÁ½ÖÖ·½·¨¿ÉÒÔÅÀµ½Â¥¶¥¡£
-		1.  1 ½× + 1 ½×
-		2.  2 ½×
+	ç¤ºä¾‹ 1ï¼š	
+		è¾“å…¥ï¼š 2
+		è¾“å‡ºï¼š 2
+		è§£é‡Šï¼š æœ‰ä¸¤ç§æ–¹æ³•å¯ä»¥çˆ¬åˆ°æ¥¼é¡¶ã€‚
+		1.  1 é˜¶ + 1 é˜¶
+		2.  2 é˜¶
 		
-	Ê¾Àı 2£º	
-		ÊäÈë£º 3
-		Êä³ö£º 3
-		½âÊÍ£º ÓĞÈıÖÖ·½·¨¿ÉÒÔÅÀµ½Â¥¶¥¡£
-		1.  1 ½× + 1 ½× + 1 ½×
-		2.  1 ½× + 2 ½×
-		3.  2 ½× + 1 ½×
+	ç¤ºä¾‹ 2ï¼š	
+		è¾“å…¥ï¼š 3
+		è¾“å‡ºï¼š 3
+		è§£é‡Šï¼š æœ‰ä¸‰ç§æ–¹æ³•å¯ä»¥çˆ¬åˆ°æ¥¼é¡¶ã€‚
+		1.  1 é˜¶ + 1 é˜¶ + 1 é˜¶
+		2.  1 é˜¶ + 2 é˜¶
+		3.  2 é˜¶ + 1 é˜¶
 */
 public class ClimbingStairs {
-	// 70.ÅÀÂ¥Ìİ
-	public static int climbStairs(int n) {
-		int[] Stairs = new int[n];
-		if (n == 1) {
-			return 1;
-		} else if (n == 2) {
+	// 70.çˆ¬æ¥¼æ¢¯
+
+	// æš´åŠ›æ³•:Oï¼ˆ2^nï¼‰
+	public int climbStairs0(int n) {
+		if (n <= 2) {
 			return 2;
 		}
-		Stairs[0] = 1;
-		Stairs[1] = 2;
-		for (int i = 2; i < n; i++) {
-			Stairs[i] = Stairs[i - 1] + Stairs[i - 2];
+		return climbStairs0(n - 1) + climbStairs0(n - 2);
+	}
+
+	// hashè®°å¿†åŒ–é€’å½’:O(n)
+	private HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+	public int climbStairs1(int n) {
+		if (map.containsKey(n)) {
+			return map.get(n);
 		}
-		return Stairs[n - 1];
+		if (n <= 2) {
+			return n;
+		}
+		int res = climbStairs1(n - 1) + climbStairs1(n - 2);
+		map.put(n, res);
+		return res;
+	}
+
+	// åŠ¨æ€è§„åˆ’O(N)+O(N)
+	public int climbStairs2(int n) {
+		if (n <= 2) {
+			return n;
+		}
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
+	}
+
+	// è¿­ä»£ï¼šæ–æ³¢é‚£å¥‘æ•°(N-1,N-2,N)
+	public int climbStairs3(int n) {
+		if (n <= 2) {
+			return n;
+		}
+		int first = 1;
+		int second = 2;
+		for (int i = 3; i <= n; i++) {
+			int temp = second;
+			second = first + second;
+			first = temp;
+		}
+		return second;
+	}
+
+	// Binets æ–¹æ³•(çŸ©é˜µä¹˜æ³•)
+
+//	 Q=[ Fn+1,Fn  ]  
+//	   [ Fn  ,Fnâˆ’1]    
+//	     ç¬¬ nä¸ªæ–æ³¢é‚£å¥‘æ•°å¯ä»¥ç”± Q^{n-1}[0,0]ç»™å‡º
+//	     
+//	  è§£é‡Šï¼š
+//		[ Fn+2,Fn+1] = [ Fn+1 + Fn  ,Fn+1 + 0*Fn] = [ Fn+1,Fn  ]  [1,1]
+//	   	[ Fn+1,Fn  ]   [ Fn   + Fn-1,Fn   + 0*Fn]   [ Fn  ,Fn-1]  [1,0]	
+//	   	Fn+1 = Q^n[0,0]
+
+	public int climbStairs4(int n) {
+		int[][] q = { { 1, 1 }, { 1, 0 } };
+		int[][] res = pow(q, n);
+		return res[0][0];
+	}
+
+	/**
+	 * çŸ©é˜µå¿«é€Ÿå¹‚
+	 * 
+	 * @param a
+	 * @param n
+	 * @return
+	 */
+	public int[][] pow(int[][] a, int n) {
+		int[][] ret = { { 1, 0 }, { 0, 1 } };
+		while (n > 0) {
+			if ((n & 1) == 1) {
+				ret = multiply(ret, a);
+			}
+			n >>= 1;
+			a = multiply(a, a);
+		}
+		return ret;
+	}
+
+	/**
+	 * çŸ©é˜µä¹˜æ³•
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int[][] multiply(int[][] a, int[][] b) {
+		int[][] c = new int[2][2];
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+			}
+		}
+		return c;
+	}
+
+	// é€šé¡¹å…¬å¼æ³•
+	public int climbStairs5(int n) {
+		double sqrt5 = Math.sqrt(5);
+		double fibn = Math.pow((1 + sqrt5) / 2, n + 1) - Math.pow((1 - sqrt5) / 2, n + 1);
+		return (int) (fibn / sqrt5);
+	}
+
+	// å°¾é€’å½’
+	public int climbStairs6(int n) {
+		if (n <= 2) {
+			return n;
+		}
+		return climbStairs6(n, 1, 2, 3);
+	}
+
+	public int climbStairs6(int n, int first, int second, int i) {
+		if (i == n) {
+			return first + second;
+		}
+		return climbStairs6(n, second, first + second, i + 1);
 	}
 	
-	//µİ¹é(³¬Ê±)
-    public int climbStairs0(int n) {
-        if(n<=2) {
-        	return 2;
-        }
-        return climbStairs0(n-1)+climbStairs0(n-2);
-    }
-    
-    //hash¼ÇÒä
-    private HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-    public int climbStairs2(int n) {
-        if(map.containsKey(n)){
-            return map.get(n);
-        }
-        if(n<=2) {
-        	return n;
-        }
-        int res =  climbStairs(n-1)+climbStairs(n-2);
-        map.put(n,res);
-        return res;
-    }
+	
+
 }
