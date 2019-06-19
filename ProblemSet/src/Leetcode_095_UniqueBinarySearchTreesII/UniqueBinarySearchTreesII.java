b77d7d769c08692eb3a52a04d48a02e205c72770
andrew <1124913956@qq.com>
2019-06-19 22:41:01 +0800

    95. 不同的二叉搜索树 II

---
+++ ProblemSet/src/Leetcode_095_UniqueBinarySearchTreesII/UniqueBinarySearchTreesII.java
@@ -0,0 +1,43 @@
+package Leetcode_095_UniqueBinarySearchTreesII;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import TreeNode.TreeNode;
+//95. 不同的二叉搜索树 II
+public class UniqueBinarySearchTreesII {
+	public List<TreeNode> generateTrees(int n) {
+		if (n == 0) {
+			return new LinkedList<TreeNode>();
+		}
+		return generate_trees(1, n);
+	}
+
+	public LinkedList<TreeNode> generate_trees(int start, int end) {
+		LinkedList<TreeNode> all_trees = new LinkedList<TreeNode>();
+		if (start > end) {
+			all_trees.add(null);
+			return all_trees;
+		}
+
+		// pick up a root
+		for (int i = start; i <= end; i++) {
+			// all possible left subtrees if i is choosen to be a root
+			LinkedList<TreeNode> left_trees = generate_trees(start, i - 1);
+
+			// all possible right subtrees if i is choosen to be a root
+			LinkedList<TreeNode> right_trees = generate_trees(i + 1, end);
+
+			// connect left and right trees to the root i
+			for (TreeNode l : left_trees) {
+				for (TreeNode r : right_trees) {
+					TreeNode current_tree = new TreeNode(i);
+					current_tree.left = l;
+					current_tree.right = r;
+					all_trees.add(current_tree);
+				}
+			}
+		}
+		return all_trees;
+	}
+}
