4fa9235cf6eba3d595f415bcbc76b3250c4cec5a
Andrew <Andrew@Andrew-pc>
2019-06-19 16:22:05 +0800

    50.Pow(x,n) 暴力 + 递归/迭代快速幂

--- ProblemSet/src/Leetcode_050_Powxn/Powxn.java
+++ ProblemSet/src/Leetcode_050_Powxn/Powxn.java
@@ -29,36 +29,81 @@ public class Powxn {
 		int n = 12;
 		Powxn pxn = new Powxn();
 		System.out.println(pxn.myPow(x, n));
-	
+
 	}
 
 	// 50.Pow(x,n)
 	public double myPow(double x, int n) {
-		
-		if (n == 0 || x == 1) {
+		if (n == 0 || x == 1)
 			return 1;
-		}
-	
-		if (n == Integer.MIN_VALUE && x == -1) {
+		if (n == Integer.MIN_VALUE && x == -1)
 			return 1;
-		}
-		if (n == Integer.MIN_VALUE) {
+		if (n == Integer.MIN_VALUE)
 			return 0;
-		}
+
 		double num = 1.0;
 		int m = Math.abs(n);
 		while (m > 0) {
-			
-			if(m%2==1) {
-				num*=x;				
-			}
+			if (m % 2 == 1)
+				num *= x;
 			x *= x;
 			m >>= 1;
 		}
-		if (n < 0) {
+		if (n < 0)
 			num = 1.0 / num;
-		}
 		return num;
 	}
 
+	// 暴力法（超时）
+	public double myPow0(double x, int n) {
+		long N = n;// -MIN = MAX-1 防止整型越界
+		if (N < 0) {
+			x = 1 / x;
+			N = -N;
+		}
+		double ans = 1;
+		for (long i = 0; i < N; i++)
+			ans = ans * x;
+		return ans;
+	}
+
+	// 递归快速幂
+	private double fastPow(double x, long n) {
+		if (n == 0) {
+			return 1.0;
+		}
+		double half = fastPow(x, n / 2);
+		if (n % 2 == 0) {
+			return half * half;
+		} else {
+			return half * half * x;
+		}
+	}
+
+	public double myPow1(double x, int n) {
+		long N = n;
+		if (N < 0) {
+			x = 1 / x;
+			N = -N;
+		}
+		return fastPow(x, N);
+	}
+
+	// 迭代快速幂
+	public double myPow2(double x, int n) {
+		long N = n;
+		if (N < 0) {
+			x = 1 / x;
+			N = -N;
+		}
+		double ans = 1;
+		double current_product = x;
+		for (long i = N; i > 0; i /= 2) {
+			if ((i % 2) == 1) {
+				ans *= current_product;
+			}
+			current_product *= current_product;
+		}
+		return ans;
+	}
 }
