fea316c63ab7b438817a40e11cb5ea831e82b4d4
ontheway12138 <1358227862@qq.com>
2019-06-26 17:24:12 +0800

    1079. 活字印刷   统计出现次数，并根据出现次数来计算

--- AnswerByCpp/1079_Letter Tile Possibilities.cpp
+++ AnswerByCpp/1079_Letter Tile Possibilities.cpp
@@ -1,11 +1,8 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
 #include<unordered_set>
-#include<numeric>
 #include<algorithm>
 using namespace std;
-
 /*
 1079. 活字印刷
 
@@ -20,18 +17,39 @@ using namespace std;
 
 class Solution {
 public:
+	// AAB :  A; AA;AAB; AB;ABA;  B;BA;BAA;  8 次  
+	int getNum(vector<int>& cnt, int num){
+		if(num <= 1) return num;
+		int res = 0;
+		// 每一个字母只会进入一次
+		for(auto &val : cnt)
+			if(val > 0) // 当前组成的字母  所以 +1
+				val--, res += getNum(cnt, num-1) + 1, val++;
+			
+		return res;
+	}
     int numTilePossibilities(string tiles) {
-        
+    	vector<int> cnt(26, 0);
+    	for(auto it:tiles) cnt[it-'A']++;
+    	return getNum(cnt, tiles.size());
+    	/*
+        unordered_set<string> hash;
+        // 先排序，再通过 next_permutation 来查看每种排列的情况
+        // 通过 hash 去重
+        sort(tiles.begin(), tiles.end());
+        while(1){
+            for(int i=0; i<tiles.size(); i++)
+                hash.insert(tiles.substr(0, i+1));
+            if(!next_permutation(tiles.begin(), tiles.end()))
+                break;
+        }
+        return hash.size();
+        */
     }
 };
 
 int main(){
-    string a = "leetcode", b = "programs", s = "sourcecode";
-    Solution* so = new Solution();
-    string res = so->smallestEquivalentString(a,b,s);
+    int res = Solution().numTilePossibilities("AAC");
     cout<<res<<endl;
-
     return 0;
 }
-1080_Insufficient Nodes in Root to Leaf Paths
-1081_Smallest Subsequence of Distinct Characters
