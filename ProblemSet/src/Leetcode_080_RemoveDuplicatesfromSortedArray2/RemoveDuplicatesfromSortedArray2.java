830439889e8035a500dc11d156141a303c6fbca7
andrew <1124913956@qq.com>
2019-06-27 11:13:10 +0800

    80. 删除排序数组中的重复项 II 明确变量定义

--- ProblemSet/src/Leetcode_080_RemoveDuplicatesfromSortedArray2/RemoveDuplicatesfromSortedArray2.java
+++ ProblemSet/src/Leetcode_080_RemoveDuplicatesfromSortedArray2/RemoveDuplicatesfromSortedArray2.java
@@ -1,4 +1,5 @@
 package Leetcode_080_RemoveDuplicatesfromSortedArray2;
+
 /*
 	给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
 	不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
@@ -31,42 +32,60 @@ public class RemoveDuplicatesfromSortedArray2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RemoveDuplicatesfromSortedArray2 rdfsa2 = new RemoveDuplicatesfromSortedArray2();
-		int[] nums = {1,2,2};
+		int[] nums = { 1, 2, 2 };
 		rdfsa2.removeDuplicates(nums);
 	}
-	//80. 删除排序数组中的重复项 II
-    public int removeDuplicates(int[] nums) {
-    	if(nums.length<=2) {
-    		return nums.length;
-    	}
-    	int i = 0;
-    	int j = 1;
-    	int count = 1;    
-    	int length = 0;
-    	while(i<nums.length-length-1) {
-    		if(nums[j]==nums[j-1]) {
-    			count++;    			
-    		}else {
-    			count = 1;
-    		}
-    		//判断是否大于2
-    		if(count >2) {
-    			move(nums,j);
-    			length++;
-    		}else {
-    			j++;
-        		i++;
-    		}
-    		    		
-    	}
-    	return nums.length-length;
-    	
-    }
-    //向前移动
-    public void move(int[] nums, int i) {
-    	for(int j = i+1;j<nums.length;j++) {
-    		nums[j-1] = nums[j];
-    	}    	
-    }
+
+	// 80. 删除排序数组中的重复项 II
+	public int removeDuplicates(int[] nums) {
+		if (nums.length <= 2) {
+			return nums.length;
+		}
+		int i = 0;
+		int j = 1;
+		int count = 1;
+		int length = 0;
+		while (i < nums.length - length - 1) {
+			if (nums[j] == nums[j - 1]) {
+				count++;
+			} else {
+				count = 1;
+			}
+			// 判断是否大于2
+			if (count > 2) {
+				move(nums, j);
+				length++;
+			} else {
+				j++;
+				i++;
+			}
+
+		}
+		return nums.length - length;
+
+	}
+
+	// 向前移动
+	public void move(int[] nums, int i) {
+		for (int j = i + 1; j < nums.length; j++) {
+			nums[j - 1] = nums[j];
+		}
+	}
+
+	// 明确变量定义，循环不变量
+	public int removeDuplicates2(int[] nums) {
+		int k = 0;// [0,k]表示不包含重复数字的范围
+		int count = 1;// count表示nums[k]出现的次数
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[k]) {
+				count = 1;
+				nums[++k] = nums[i];
+			} else if (count < 2) {
+				nums[++k] = nums[i];
+				count++;
+			}
+		}
+		return k + 1;
+	}
 
 }
