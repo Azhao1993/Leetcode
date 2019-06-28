fc31657e9c6e5582ee1ddb0a2c71bb1e7bee0def
andrew <1124913956@qq.com>
2019-06-28 21:36:15 +0800

    16.最接近的三数之和 确定a 找b,c

--- ProblemSet/src/Leetcode_016_3SumClosest/ThreeSumClosest.java
+++ ProblemSet/src/Leetcode_016_3SumClosest/ThreeSumClosest.java
@@ -18,30 +18,6 @@ public class ThreeSumClosest {
 	}
 
 	// 16.最接近的三数之和
-	public int threeSumClosest(int[] nums, int target) {
-		Arrays.sort(nums);
-		int sum = 0;
-		int dis = Integer.MAX_VALUE;
-		for (int x = 0; x < nums.length - 2; x++) {
-			for (int y = x + 1; y < nums.length - 1; y++) {
-				for (int z = y + 1; z < nums.length; z++) {
-					int temp = nums[x] + nums[y] + nums[z];
-					// System.out.println(nums[x] + "+" + nums[y] + "+" + nums[z] + ":" + temp);
-					if (Math.abs(temp - target) <= dis) {
-						sum = temp;
-						dis = Math.abs(temp - target);
-					} else if (temp <= target) {
-						continue;
-					} else {
-						break;
-					}
-				}
-				// System.out.println();
-			}
-		}
-		return sum;
-	}
-
 	// O(n^2)
 	public int threeSumClosest0(int[] num, int target) {
 		int result = num[0] + num[1] + num[num.length - 1];
