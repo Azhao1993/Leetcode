0658375ef7bff05a96ebc4fb2dd8d925f0ba34cc
andrew <1124913956@qq.com>
2019-07-02 13:57:43 +0800

    203. 移除链表元素 假头

--- ProblemSet/src/Leetcode_203_RemoveLinkedListElements/RemoveLinkedListElements.java
+++ ProblemSet/src/Leetcode_203_RemoveLinkedListElements/RemoveLinkedListElements.java
@@ -31,52 +31,26 @@ public class RemoveLinkedListElements {
 
 	// 203. 移除链表元素
 	public ListNode removeElements(ListNode head, int val) {
-		// 空链表
-		if (head == null) {
+
+	// 假头
+	public ListNode removeElements2(ListNode head, int val) {
+		if (head == null || head.next == null && head.val == val) {
 			return null;
 		}
-		// 只有一个节点
-		if (head.next == null) {
+		ListNode preHead = new ListNode(0);// 假头
+		ListNode pre = preHead;
+		preHead.next = head;
+		ListNode next = head.next;
+		while (head != null) {
+			next = head.next;
 			if (head.val == val) {
-				return null;
+				pre.next = next;
 			} else {
-				return head;
+				pre = pre.next;
 			}
+			head = next;
 		}
-		// 两个或者两个以上的节点
-		ListNode newHead = new ListNode(0);
-		newHead.next = head;
-		while ((newHead != null) && (newHead.next != null)) {
-			if (newHead.next.val != val) {
-				newHead = newHead.next;
-			} else {
-				if (newHead.next == head) {
-					head = head.next;
-					newHead.next = head;
-				} else {
-					newHead.next = newHead.next.next;
-				}
-
-			}
-		}
-
-		return head;
-
+		return preHead.next;
 	}
 
-	// 3ms
-	public ListNode removeElements0(ListNode head, int val) {
-		ListNode dummy = new ListNode(0);
-		dummy.next = head;
-		head = dummy;
-
-		while (head.next != null) {
-			if (head.next.val == val) {
-				head.next = head.next.next;
-			} else {
-				head = head.next;
-			}
-		}
-		return dummy.next;
-	}
 }
