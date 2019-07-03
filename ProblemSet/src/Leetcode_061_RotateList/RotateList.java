fec8749bea8e95132e9a88bc22f358b3958989b5
andrew <1124913956@qq.com>
2019-07-03 19:30:43 +0800

    61. 旋转链表

--- ProblemSet/src/Leetcode_061_RotateList/RotateList.java
+++ ProblemSet/src/Leetcode_061_RotateList/RotateList.java
@@ -1,6 +1,7 @@
 package Leetcode_061_RotateList;
 
 import LinkedList.ListNode;
+import LinkedList.ListNodeUtils;
 
 /*
 	给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
@@ -23,90 +24,50 @@ import LinkedList.ListNode;
  */
 public class RotateList {
 	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
-		ListNode node2 = new ListNode(2);
-		ListNode node3 = new ListNode(3);
-		ListNode node4 = new ListNode(4);
-		ListNode node5 = new ListNode(5);
-
-		head.next = node2;
-		node2.next = node3;
-		node3.next = node4;
-		node4.next = node5;
-
-		int k = 2;
-
-		RotateList rl = new RotateList();
-		System.out.println(rl.getListNodeLength(head));
+		ListNode head = ListNodeUtils.creatNode(new int[] { 1 }, 1);
+		ListNodeUtils.printList(head);
+		head = new RotateList().rotateRight(head, 0);
+		ListNodeUtils.printList(head);
 
 	}
 
 	// 61. 旋转链表
 	public ListNode rotateRight(ListNode head, int k) {
-		if ((head == null) || (head.next == null)) {
+		if (head == null || head.next == null) {
 			return head;
 		}
-		int length = getListNodeLength(head);
-		for (int i = 0; i < k % (length - 1); i++) {
-			head = rotateOne(head);
-		}
-		return head;
 
-	}
-
-	// 旋转1次
-	public ListNode rotateOne(ListNode head) {
-		ListNode headnext = head;
-		while (head.next.next != null) {
-			head = head.next;
-		}
-		ListNode last = head.next;
-		head.next = null;
-		last.next = headnext;
-		head = last;
-
-		return head;
-	}
-
-	// 获取链表长度
-	private int getListNodeLength(ListNode head) {
-		int length = 0;
-		while (head != null) {
-			length++;
-			head = head.next;
-		}
-		return length;
-	}
-
-	// 7ms
-	public ListNode rotateRight0(ListNode head, int k) {
-		if (head == null) {
-			return head;
-		}
-		int len = 0;
 		ListNode p = head;
+
+		int length = 0;
 		while (p != null) {
-			len++;
+			++length;
 			p = p.next;
 		}
-		int n = k % len;
-		if (n == 0) {
+		k %= length;
+		if (k == 0) {
 			return head;
 		}
-		// System.out.println(len);
-		int l = len - n - 1;
-		ListNode dummy = head, curr = head;
-		while (l >= 1) {
-			curr = curr.next;
-			l--;
+
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+
+		p = dummy;
+		ListNode q = dummy;
+
+		for (int i = 0; i < k; i++) {
+			q = q.next;
 		}
 
-		ListNode t = curr.next, res = curr.next;
-		curr.next = null;
-		while (t.next != null) {
-			t = t.next;
+		while (q.next != null) {
+			p = p.next;
+			q = q.next;
 		}
-		t.next = dummy;
-		return res;
+
+		dummy.next = p.next;
+		p.next = null;
+		q.next = head;
+
+		return dummy.next;
 	}
 }
