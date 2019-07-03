98f8814704c1ad484dc579f455786ef0c264455d
andrew <1124913956@qq.com>
2019-07-03 11:05:43 +0800

    203. 移除链表元素

--- ProblemSet/src/Leetcode_203_RemoveLinkedListElements/RemoveLinkedListElements.java
+++ ProblemSet/src/Leetcode_203_RemoveLinkedListElements/RemoveLinkedListElements.java
@@ -10,24 +10,6 @@ import LinkedList.ListNode;
 		输出: 1->2->3->4->5
  */
 public class RemoveLinkedListElements {
-	public static void main(String[] args) {
-		ListNode head = new ListNode(1);
-		ListNode node2 = new ListNode(2);
-		ListNode node3 = new ListNode(2);
-		ListNode node4 = new ListNode(1);
-		// ListNode node5 = new ListNode(4);
-		// ListNode node6 = new ListNode(5);
-		// ListNode node7 = new ListNode(6);
-		head.next = node2;
-		node2.next = node3;
-		node3.next = node4;
-		// node4.next = node5;
-		// node5.next = node6;
-		// node6.next = node7;
-
-		RemoveLinkedListElements rlle = new RemoveLinkedListElements();
-		rlle.removeElements(head, 6);
-	}
 
 	// 203. 移除链表元素
 	// 假头
