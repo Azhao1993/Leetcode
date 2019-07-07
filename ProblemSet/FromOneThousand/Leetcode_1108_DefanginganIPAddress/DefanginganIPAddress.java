54e1b1c8ba9bf7682164d072b33f569b9ed18432
andrew <1124913956@qq.com>
2019-07-07 15:50:46 +0800

    1108. IP地址无效化

---
+++ ProblemSet/FromOneThousand/Leetcode_1108_DefanginganIPAddress/DefanginganIPAddress.java
@@ -0,0 +1,30 @@
+package Leetcode_1108_DefanginganIPAddress;
+
+/*
+	给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。	
+	所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。	 
+	
+	示例 1：	
+		输入：address = "1.1.1.1"
+		输出："1[.]1[.]1[.]1"
+	示例 2：	
+		输入：address = "255.100.50.0"
+		输出："255[.]100[.]50[.]0"	 
+	
+	提示：	
+		给出的 address 是一个有效的 IPv4 地址
+ */
+public class DefanginganIPAddress {
+	// 1108.IP 地址无效化
+	public String defangIPaddr(String address) {
+		StringBuilder res = new StringBuilder();
+		for (char ch : address.toCharArray()) {
+			if (ch != '.') {
+				res.append(ch);
+			} else {
+				res.append("[.]");
+			}
+		}
+		return res.toString();
+	}
+}
