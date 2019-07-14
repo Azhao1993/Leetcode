095453c829819f7ad29d5eaecd6688bb2892b71b
ontheway12138 <1358227862@qq.com>
2019-07-14 09:16:37 +0800

    1121. 将数组分成几个递增序列  重复最多的元素

---
+++ AnswerByCpp/1121_Divide Array Into Increasing Sequences.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/*
+1121. 将数组分成几个递增序列
+
+给你一个 非递减 的正整数数组 nums 和整数 K，判断该数组是否可以被分成一个或几个 长度至少 为 K 的 不相交的递增子序列。
+
+示例 1：       输入：nums = [1,2,2,3,3,4,4], K = 3        输出：true
+解释：     该数组可以分成两个子序列 [1,2,3,4] 和 [2,3,4]，每个子序列的长度都至少是 3。
+示例 2：       输入：nums = [5,6,6,7,8], K = 3            输出：false
+解释：     没有办法根据条件来划分数组。 
+
+提示：     1 <= nums.length <= 10^5        1 <= K <= nums.length       1 <= nums[i] <= 10^5
+*/
+
+class Solution {
+public:
+    bool canDivideIntoSubsequences(vector<int>& nums, int K) {
+        int len = nums.size(), iMax = 1, cur = 1;
+        for(int i=1; i<len; i++){
+            if(nums[i] == nums[i-1]) 
+                cur++, iMax = max(iMax, cur);
+            else cur = 1;
+        }
+        return len >= iMax * K;
+    }
+};
+
+int main(){
+    vector<int> arr{5,6,6,7,8};
+    bool bl = Solution().canDivideIntoSubsequences(arr, 2);
+    cout<<bl<<endl;
+    return 0;
+}
