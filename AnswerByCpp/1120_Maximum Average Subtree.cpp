e852ce60abf7772310d23bac70613ac95c73ba02
ontheway12138 <1358227862@qq.com>
2019-07-14 08:58:50 +0800

    1120. 子树的最大平均值  后序遍历  递归

---
+++ AnswerByCpp/1120_Maximum Average Subtree.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include<algorithm>
+#include<stack>
+using namespace std;
+/*
+1120. 子树的最大平均值
+
+给你一棵二叉树的根节点 root，找出这棵树的 每一棵 子树的 平均值 中的 最大 值。
+子树是树中的任意节点和它的所有后代构成的集合。
+树的平均值是树中节点值的总和除以节点数。
+
+示例：       输入：[5,6,1]      输出：6.00000
+解释： 以 value = 5 的节点作为子树的根节点，得到的平均值为 (5 + 6 + 1) / 3 = 4。
+以 value = 6 的节点作为子树的根节点，得到的平均值为 6 / 1 = 6。
+以 value = 1 的节点作为子树的根节点，得到的平均值为 1 / 1 = 1。      所以答案取最大值 6。
+
+提示：     树中的节点数介于 1 到 5000之间。        每个节点的值介于 0 到 100000 之间。
+如果结果与标准答案的误差不超过 10^-5，那么该结果将被视为正确答案。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    double maximumAverageSubtree(TreeNode* root) {
+        int num = 0, sum = 0;
+        return helper(root, num, sum);
+    }
+    double helper(TreeNode* root, int& num, int& sum){
+        if(root == NULL) return 0.0;
+        int leftNum = 0, rightNum = 0, leftSum = 0, rightSum = 0;
+        double leftRes = helper(root->left, leftNum, leftSum);
+        double rightRes = helper(root->right, rightNum, rightSum);
+
+        num = leftNum + rightNum + 1, sum = leftSum + rightSum + root->val;
+        double res = max( (double)sum/num, max(leftRes, rightRes));
+        return res;
+    }
+};
