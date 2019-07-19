4713c166b99f4802ed0382ad9fc905cbd111096a
andrew <1124913956@qq.com>
2019-07-19 15:20:58 +0800

    236. 二叉树的最近公共祖先

--- ProblemSet/src/Leetcode_236_LowestCommonAncestorofaBinaryTree/LowestCommonAncestorofaBinaryTree.java
+++ ProblemSet/src/Leetcode_236_LowestCommonAncestorofaBinaryTree/LowestCommonAncestorofaBinaryTree.java
@@ -28,71 +28,24 @@ import TreeNode.TreeNode;
  */
 public class LowestCommonAncestorofaBinaryTree {
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(3);
-		TreeNode node5 = new TreeNode(5);
-		TreeNode node1 = new TreeNode(1);
-		TreeNode node6 = new TreeNode(6);
-		TreeNode node2 = new TreeNode(2);
-		TreeNode node0 = new TreeNode(0);
-		TreeNode node8 = new TreeNode(8);
-		TreeNode node7 = new TreeNode(7);
-		TreeNode node4 = new TreeNode(4);
-		root.left = node5;
-		root.right = node1;
-		node5.left = node6;
-		node5.right = node2;
-		node1.left = node0;
-		node1.right = node8;
-		node2.left = node7;
-		node2.right = node4;
 		LowestCommonAncestorofaBinaryTree lcabt = new LowestCommonAncestorofaBinaryTree();
-		lcabt.lowestCommonAncestor(root, node7, node4);
+		lcabt.lowestCommonAncestor(null, null, null);
 	}
 
 	// 236. 二叉树的最近公共祖先
-
-	// 先从上往下遍历，再从下往上回溯
-
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 		// 其中一个为root
 		if (root == null || root.val == p.val || root.val == q.val) {
 			return root;
 		}
-		//
-		TreeNode leftN = lowestCommonAncestor(root.left, p, q);
-		TreeNode rightN = lowestCommonAncestor(root.right, p, q);
-		if (leftN != null && rightN != null) {
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left != null && right != null) {
 			return root;
 		}
-		if (leftN == null) {
-			return rightN;
+		if (left == null) {
+			return right;
 		}
-		return leftN;
-	}
-
-	// 7ms
-	private TreeNode ans;
-
-	public LowestCommonAncestorofaBinaryTree() {
-		this.ans = null;
-	}
-
-	public boolean recurseTree(TreeNode curr, TreeNode p, TreeNode q) {
-		if (curr != null) {
-			int left = this.recurseTree(curr.left, p, q) ? 1 : 0;
-			int right = this.recurseTree(curr.right, p, q) ? 1 : 0;
-			int mid = (curr == p) || (curr == q) ? 1 : 0;
-			if ((left + right + mid) >= 2) {
-				this.ans = curr;
-			}
-			return (mid + left + right > 0);
-
-		} else
-			return false;
-	}
-
-	public TreeNode lowestCommonAncestor0(TreeNode root, TreeNode p, TreeNode q) {
-		this.recurseTree(root, p, q);
-		return this.ans;
+		return left;
 	}
 }
