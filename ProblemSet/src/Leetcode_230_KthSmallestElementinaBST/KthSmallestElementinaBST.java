572fecb79d83c8858c704446fa30b87a18b83b49
andrew <1124913956@qq.com>
2019-07-19 14:45:14 +0800

    230.二叉搜索树中第K小的元素 利用中序遍历记录节点个数

--- ProblemSet/src/Leetcode_230_KthSmallestElementinaBST/KthSmallestElementinaBST.java
+++ ProblemSet/src/Leetcode_230_KthSmallestElementinaBST/KthSmallestElementinaBST.java
@@ -1,6 +1,7 @@
 package Leetcode_230_KthSmallestElementinaBST;
 
 import TreeNode.TreeNode;
+import TreeNode.TreeNodeUtils;
 
 /*
 	给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
@@ -32,21 +33,37 @@ import TreeNode.TreeNode;
 
 //230.二叉搜索树中第K小的元素
 public class KthSmallestElementinaBST {
-	int count = 0;
-	int result = Integer.MIN_VALUE;
-
-	public int kthSmallest(TreeNode root, int k) {
-		traverse(root, k);
-		return result;
+	public static void main(String[] args) {
+		String[] arr = { "3", "1", "4", "null", "2" };
+		TreeNode root = TreeNodeUtils.createTree(arr);
+		TreeNodeUtils.printTree(root);
+		new KthSmallestElementinaBST().kthSmallest(root, 1);
 	}
 
-	public void traverse(TreeNode root, int k) {
-		if (root == null)
+	public int kthSmallest(TreeNode root, int k) {
+		int[] res = new int[2];
+		// res[0]当前已经遍历了多少节点
+		// res[1]第k个值是多少
+		traverse(root, k, res);
+		return res[1];
+	}
+
+	// 中序遍历，查找记录左子树有多少个节点
+	public void traverse(TreeNode root, int k, int[] res) {
+		if (root == null) {
 			return;
-		traverse(root.left, k);
-		count++;
-		if (count == k)
-			result = root.val;
-		traverse(root.right, k);
+		}
+
+		// 判断加速
+		if (res[0] < k) {
+			traverse(root.left, k, res);
+			res[0]++;
+			if (res[0] == k) {
+				res[1] = root.val;
+				return;
+			}
+			traverse(root.right, k, res);
+		}
+
 	}
 }
