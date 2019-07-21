4c6a94ec22c32c1c5ae3e3edbd228c909337e5d0
andrew <1124913956@qq.com>
2019-07-21 22:32:46 +0800

    93. 复原IP地址

--- ProblemSet/src/Leetcode_093_RestoreIPAddresses/RestoreIPAddresses.java
+++ ProblemSet/src/Leetcode_093_RestoreIPAddresses/RestoreIPAddresses.java
@@ -1,6 +1,7 @@
 package Leetcode_093_RestoreIPAddresses;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /*
@@ -14,18 +15,36 @@ import java.util.List;
 	链接：https://leetcode-cn.com/problems/restore-ip-addresses
 	著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+//93. 复原IP地址
 public class RestoreIPAddresses {
-    public List<String> restoreIpAddresses(String s) {
-        List<String> res = new ArrayList<>();
-        if(s==null||s.length()<4) {
-        	return res;
-        }
-        restoreIpAddresses(s,1,1,1,1,res);
-        
-        return res;
-    }
+	List<String> res = new ArrayList<>();
+	String s;
 
-	private void restoreIpAddresses(String s, int i, int j, int k, int l, List<String> res) {
-				
+	public List<String> restoreIpAddresses(String s) {
+		if (s == null || s.length() == 0) {
+			return res;
+		}
+		this.s = s;
+		dfs(0, "", 0);
+		return res;
+	}
+
+	private void dfs(int curLength, String cur, int count) {
+		if (count > 4) {
+			return;
+		}
+		if (count == 4 && curLength == s.length()) {
+			res.add(cur);
+		}
+		for (int i = 1; i < 4; i++) {
+			if (curLength + i > s.length()) {
+				break;
+			}
+			String str = s.substring(curLength, curLength + i);
+			if ((str.startsWith("0") && str.length() > 1) || (i == 3 && Integer.valueOf(str) >= 256)) {
+				continue;
+			}
+			dfs(curLength + i, cur + str + (count == 3 ? "" : "."), count + 1);
+		}
 	}
 }
