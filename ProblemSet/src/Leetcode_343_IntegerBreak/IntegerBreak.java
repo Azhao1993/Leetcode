ac0a4f31e6a3c0a4226939ab70bdae5e98c90287
andrew <1124913956@qq.com>
2019-07-22 14:04:12 +0800

    343. 整数拆分 递归+记忆化搜索+动态规划

--- ProblemSet/src/Leetcode_343_IntegerBreak/IntegerBreak.java
+++ ProblemSet/src/Leetcode_343_IntegerBreak/IntegerBreak.java
@@ -1,5 +1,7 @@
 package Leetcode_343_IntegerBreak;
 
+import java.util.Arrays;
+
 /*
 	给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
 	
@@ -21,19 +23,60 @@ package Leetcode_343_IntegerBreak;
 
 //343. 整数拆分
 public class IntegerBreak {
-	public int integerBreak(int n) {
-		if (n <= 1) {
-			return 0;
+	public static void main(String[] args) {
+		new IntegerBreak().integerBreak1(10);
+	}
+
+	// 递归(超时)
+	public int integerBreak2(int n) {
+		return breakInteger(n);
+	}
+
+	private int breakInteger(int n) {
+		if (n == 1) {
+			return 1;
 		}
+		int res = -1;
+		for (int i = 1; i <= n - 1; i++) {
+			res = Math.max(res, Math.max(i * (n - i), i * breakInteger(n - i)));
+		}
+
+		return res;
+	}
+
+	// 记忆化搜索
+	public int integerBreak1(int n) {
+		int[] memo = new int[n + 1];
+		Arrays.fill(memo, -1);
+		return breakInteger(n, memo);
+	}
+
+	private int breakInteger(int n, int[] memo) {
+		if (n == 1) {
+			return 1;
+		}
+
+		if (memo[n] != -1) {
+			return memo[n];
+		}
+
+		int res = -1;
+		for (int i = 1; i <= n - 1; i++) {
+			res = Math.max(res, Math.max(i * (n - i), i * breakInteger(n - i, memo)));
+		}
+		memo[n] = res;
+		return res;
+
+	}
+
+	// 动态规划
+	public int integerBreak(int n) {
 		int[] dp = new int[n + 1];
+		Arrays.fill(dp, -1);
 		for (int i = 2; i <= n; i++) {
-			int res = Integer.MIN_VALUE;
 			for (int j = 1; j <= i / 2; j++) {
-				int a = Math.max(dp[j], j);
-				int b = Math.max(dp[i - j], i - j);
-				res = Math.max(a * b, res);
+				dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
 			}
-			dp[i] = res;
 		}
 		return dp[n];
 	}
