d2beeb92667ae8e4b4d85029bd61898b0009ca6e
andrew <1124913956@qq.com>
2019-07-23 14:20:16 +0800

    63. 不同路径 II 动态规划

--- ProblemSet/src/Leetcode_063_UniquePaths2/UniquePaths2.java
+++ ProblemSet/src/Leetcode_063_UniquePaths2/UniquePaths2.java
@@ -1,7 +1,5 @@
 package Leetcode_063_UniquePaths2;
 
-
-
 /*
 	一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
 	机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
@@ -29,60 +27,46 @@ public class UniquePaths2 {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		UniquePaths2 up2 = new UniquePaths2();
-		int[][] obstacleGrid = {{0,0,0},{0,1,0},{0,0,0}};
-		int num=up2.uniquePathsWithObstacles(obstacleGrid);
+		int[][] obstacleGrid = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+		int num = up2.uniquePathsWithObstacles(obstacleGrid);
 		System.out.println(num);
 	}
-	//63. 不同路径 II
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    	//第一个位置为障碍物
-    	if(obstacleGrid[0][0]==1) {
-    		return 0;
-    	}
-        //获取行列
-    	int m = obstacleGrid.length;
-    	int n = obstacleGrid[0].length;
-    	//更新第一列
-    	for(int i=0;i<m;i++) {
-    		if(obstacleGrid[i][0]==1) {
-    			//i，0处存在障碍物    			
-    			//障碍物后面无路可走
-    			for(int p = i;p<m;p++) {
-    				obstacleGrid[p][0]=0;    				
-    			}
-    			break;
-    		}else {
-    			obstacleGrid[i][0] =1;
-    		}
-    	}
-    	//更新第一行
-    	for(int j=1;j<n;j++) {
-    		if(obstacleGrid[0][j]==1) {
-    			//i，0处存在障碍物    			
-    			//障碍物后面无路可走
-    			for(int p = j;p<n;p++) {
-    				obstacleGrid[0][p]=0;
-    			}
-    			break;
-    		}else {
-    			obstacleGrid[0][j] =1;
-    		}
-    	}
-    	//找出路径个数
-    	for(int i = 1;i<m;i++) {
-    		for(int j = 1;j<n;j++) {
-    			if(obstacleGrid[i][j]==1) {
-    				obstacleGrid[i][j]=0;
-    			}else{
-    				obstacleGrid[i][j]=obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
-    			}
-    			
-    		}
-    	}
-    	//输出二维数组
-    	//new matrixOutput().ArrayOutput(obstacleGrid);
-    	
-    	return obstacleGrid[m-1][n-1];
-    }
+
+	// 63. 不同路径 II
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		if (obstacleGrid == null || obstacleGrid.length == 0) {
+			return 0;
+		}
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+
+		int[][] dp = new int[m][n];
+		if (obstacleGrid[m - 1][n - 1] == 1) {
+			return 0;
+		} else {
+			dp[m - 1][n - 1] = 1;
+		}
+		// 最后一行
+		for (int j = n - 2; j >= 0; j--) {
+			if (obstacleGrid[m - 1][j] != 1) {
+				dp[m - 1][j] = dp[m-1][j + 1];
+			}
+		}
+		// 最后一列
+		for (int i = m - 2; i >= 0; i--) {
+			if (obstacleGrid[i][n - 1] != 1) {
+				dp[i][n - 1] = dp[i + 1][n - 1];
+			}
+		}
+		// 其他行其他列
+		for (int i = m - 2; i >= 0; i--) {
+			for (int j = n - 2; j >= 0; j--) {
+				if (obstacleGrid[i][j] != 1) {
+					dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+				}
+			}
+		}
+		return dp[0][0];
+	}
 
 }
