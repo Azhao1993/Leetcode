a086c7656004f1bc0032cd6641d054cd9244c3b9
andrew <1124913956@qq.com>
2019-09-29 18:44:16 +0800

    1115. 交替打印FooBar

---
+++ ProblemSet/FromOneThousand/Leetcode_1114_PrintinOrder/Main.java
@@ -0,0 +1,27 @@
+package Leetcode_1114_PrintinOrder;
+
+public class Main {
+	public static void main(String[] args) {
+		Runnable printFirst = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("AAA");
+			}
+		};
+		Runnable printSecond = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("BBB");
+			}
+		};
+		Runnable printThree = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("CCC");
+			}
+		};
+		
+		
+
+	}
+}
---
+++ ProblemSet/FromOneThousand/Leetcode_1115_PrintFooBarAlternately/FooBar.java
@@ -0,0 +1,72 @@
+package Leetcode_1115_PrintFooBarAlternately;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/*
+	我们提供一个类：	
+		class FooBar {
+			public void foo() {
+			    for (int i = 0; i < n; i++) {
+			      print("foo");
+			    }
+		  	}
+		
+		 	public void bar() {
+		    	for (int i = 0; i < n; i++) {
+		     		print("bar");
+		    	}
+		  	}
+		}
+	两个不同的线程将会共用一个 FooBar 实例。
+	其中一个线程将会调用 foo() 方法，另一个线程将会调用 bar() 方法。
+	
+	请设计修改程序，以确保 "foobar" 被输出 n 次。
+	
+	示例 1:
+		输入: n = 1
+		输出: "foobar"
+		解释: 这里有两个线程被异步启动。其中一个调用 foo() 方法, 另一个调用 bar() 方法，"foobar" 将被输出一次。
+	示例 2:
+		输入: n = 2
+		输出: "foobarfoobar"
+		解释: "foobar" 将被输出两次。
+	
+	来源：力扣（LeetCode）
+	链接：https://leetcode-cn.com/problems/print-foobar-alternately
+	著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+//1115. 交替打印FooBar
+public class FooBar {
+	private int n;
+	// 阻塞队列
+	private BlockingQueue<Integer> fooqueue;
+	private BlockingQueue<Integer> barqueue;
+
+	public FooBar(int n) {
+		this.n = n;
+		this.fooqueue = new LinkedBlockingQueue<Integer>(1);
+		fooqueue.add(0);
+		this.barqueue = new LinkedBlockingQueue<Integer>(1);
+	}
+
+	public void foo(Runnable printFoo) throws InterruptedException {
+		for (int i = 0; i < n; i++) {	
+			fooqueue.take();
+			printFoo.run();	
+			barqueue.put(0);
+		}
+
+	}
+
+	public void bar(Runnable printBar) throws InterruptedException {
+
+		for (int i = 0; i < n; i++) {
+			barqueue.take();
+			printBar.run();	
+			fooqueue.put(0);
+		}
+
+	}
+}
