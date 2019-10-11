6b0e7197e1ead9822dcfd0d3595af306f715e054
ontheway12138 <1358227862@qq.com>
2019-10-11 14:30:59 +0800

    281. 锯齿迭代器  新建一个 vector

---
+++ AnswerByCpp/0281_Zigzag Iterator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+using namespace std;
+/*
+281. 锯齿迭代器
+
+给出两个一维的向量，请你实现一个迭代器，交替返回它们中间的元素。
+
+示例: 输入: v1 = [1,2]  v2 = [3,4,5,6]      输出: [1,3,2,4,5,6]
+解析: 通过连续调用 next 函数直到 hasNext 函数返回 false，     next 函数返回值的次序应依次为: [1,3,2,4,5,6]。
+拓展：假如给你 k 个一维向量呢？你的代码在这种情况下的扩展性又会如何呢?
+拓展声明：  “锯齿” 顺序对于 k > 2 的情况定义可能会有些歧义。所以，假如你觉得 “锯齿” 这个表述不妥，也可以认为这是一种 “循环”。
+例如： 输入:     [1,2,3]     [4,5,6,7]   [8,9]       输出: [1,4,8,2,5,9,3,6,7].
+*/
+
+class ZigzagIterator {
+private:
+    vector<int> arr;
+    int idx, len;
+public:
+    ZigzagIterator(vector<int>& v1, vector<int>& v2) {
+        int i = 0, j = 0, len1 = v1.size(), len2 = v2.size();
+        idx = 0, len = len1 + len2;
+        while (i < len1 || j < len2) {
+            if (i < len1) arr.push_back(v1[i++]);
+            if (j < len2) arr.push_back(v2[j++]);
+        }
+    }
+
+    int next() {
+        return arr[idx++];
+    }
+
+    bool hasNext() {
+        return idx < len;
+    }
+};
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i(v1, v2);
+ * while (i.hasNext()) cout << i.next();
+ */
